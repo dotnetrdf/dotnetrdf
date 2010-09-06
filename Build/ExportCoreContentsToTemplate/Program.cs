f8a4a295c24662bd9965600c5fcc40b002db3676
Rob Vesse <rvesse@dotnetrdf.org>
2010-09-06 11:27:05 +0000

    Added a build tool to automatically generated the Silverlight and Compact Framework Project files from the core library project file whenever it is built
    Note: Compact build does not compile and we are not currently actively working on this build or planning to release it
    Note: Silverlight build requires you to use VS 2010 to build the library
    
    Silverlight, Compact Framework and Compatability Test projects are no longer in the main VS 2008 Solution

---
+++ Build/ExportCoreContentsToTemplate/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace ExportCoreContentsToTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("dotNetRDF Build Tools - Export Core Library Contents to Template");
+                Console.WriteLine();
+                Console.WriteLine("This tool is used to export the compilable contents of the dotNetRDF library to a Template Project file to create an up to date Project file for building dotNetRDF targetted at a specific platform");
+                Console.WriteLine("Usage is ExportCoreContentsToTemplate CoreProject.csproj Target.csproj.template Output.csproj [relPath]");
+                Console.WriteLine("Relative Path is ..\\core\\ by default");
+                return;
+            }
+
+            try
+            {
+                Console.WriteLine("dotNetRDF Build Tools: Attempting to generate Project " + args[2] + " using Template " + args[1] + " from Project " + args[1]);
+
+                //Load Core Project
+                XmlDocument coreProject = new XmlDocument();
+                coreProject.Load(args[0]);
+
+                //Set Relative Path
+                String relativePath = (args.Length >= 4) ? args[3] : "..\\core\\";
+
+                //Open Target Project Template
+                XmlDocument template = new XmlDocument();
+                template.Load(args[1]);
+
+                //Copy Compile Item Groups from Core to Template
+                foreach (XmlNode itemGroup in coreProject.DocumentElement.GetElementsByTagName("ItemGroup"))
+                {
+                    XmlElement newItemGroup = template.CreateElement("ItemGroup");
+
+                    foreach (XmlNode compile in itemGroup.ChildNodes)
+                    {
+                        if (compile.Name.Equals("Compile") && compile.Attributes.GetNamedItem("Include") != null)
+                        {
+                            //Never include Compiled files which are themselves linked from another project
+                            if (compile.HasChildNodes)
+                            {
+                                bool ok = true;
+                                foreach (XmlNode n in compile.ChildNodes)
+                                {
+                                    if (n.Name.Equals("Link")) ok = false;
+                                }
+                                if (!ok) continue;
+                            }
+
+                            XmlElement newCompile = template.CreateElement("Compile");
+                            XmlAttribute include = template.CreateAttribute("Include");
+                            String includeFile = compile.Attributes["Include"].Value;
+
+                            //Never include AssemblyInfo from source Project
+                            if (includeFile.EndsWith("AssemblyInfo.cs")) continue;
+
+                            include.Value = relativePath + includeFile;
+                            newCompile.Attributes.Append(include);
+
+                            XmlElement link = template.CreateElement("Link");
+                            link.InnerText = compile.Attributes["Include"].Value;
+                            newCompile.AppendChild(link);
+
+                            newItemGroup.AppendChild(newCompile);
+                        }
+                    }
+
+                    if (newItemGroup.HasChildNodes)
+                    {
+                        template.DocumentElement.AppendChild(newItemGroup);
+                    }
+                }
+
+                template.Save(args[2]);
+
+                Console.WriteLine("dotNetRDF Build Tools: Successfully generated Project " + args[2]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("dotNetRDF Build Tools: An error occurred attempting to export Core Project " + args[0] + " to Project " + args[2] + " using Template " + args[1]);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+    }
+}
