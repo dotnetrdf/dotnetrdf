0c679d78357912c388dbaf2c60492a8c6e893724
Rob Vesse <rvesse@dotnetrdf.org>
2010-11-10 12:42:55 +0000

    Added unit tests to ensure the SPARQL DESCRIBE algorithms work as expected

---
+++ Testing/unittest/Sparql/DescribeAlgorithms.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VDS.RDF.Parsing;
+using VDS.RDF.Query;
+using VDS.RDF.Query.Datasets;
+using VDS.RDF.Query.Describe;
+
+namespace VDS.RDF.Test.Sparql
+{
+    [TestClass]
+    public class DescribeAlgorithms
+    {
+        private const String DescribeQuery = "PREFIX foaf: <http://xmlns.com/foaf/0.1/> DESCRIBE ?x WHERE {?x foaf:name \"Dave\" }";
+
+        private SparqlQueryParser _parser;
+        private InMemoryDataset _data;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this._parser = new SparqlQueryParser();
+            TripleStore store = new TripleStore();
+            Graph g = new Graph();
+            FileLoader.Load(g, "describe-algos.ttl");
+            store.Add(g);
+            this._data = new InMemoryDataset(store);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            this._parser = null;
+            this._data = null;
+        }
+
+        private SparqlQuery GetQuery()
+        {
+            return this._parser.ParseFromString(DescribeQuery);
+        }
+
+        private Graph RunDescribeTest(ISparqlDescribe describer)
+        {
+            SparqlQuery q = this.GetQuery();
+            q.Describer = describer;
+            Object results = q.Evaluate(this._data);
+            if (results is Graph)
+            {
+                TestTools.ShowResults(results);
+                return (Graph)results;
+            }
+            else 
+            {
+                Assert.Fail("Expected a Graph as the Result");
+                return null;
+            }
+        }
+
+        [TestMethod]
+        public void DescribeCBD()
+        {
+            this.RunDescribeTest(new ConciseBoundedDescription());
+        }
+
+        [TestMethod]
+        public void DescribeSCBD()
+        {
+            this.RunDescribeTest(new SymmetricConciseBoundedDescription());
+        }
+
+        [TestMethod]
+        public void DescribeSubject()
+        {
+            this.RunDescribeTest(new SimpleSubjectDescription());
+        }
+
+        [TestMethod]
+        public void DescribeSubjectObject()
+        {
+            this.RunDescribeTest(new SimpleSubjectObjectDescription());
+        }
+
+        [TestMethod]
+        public void DescribeMSG()
+        {
+            this.RunDescribeTest(new MinimalSpanningGraph());
+        }
+    }
+}
