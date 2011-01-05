c4e4aba2f43cdb5dadea99fca4b9f9fd28c1397b
Rob Vesse <rvesse@dotnetrdf.org>
2011-01-05 12:34:49 +0000

    Added a patch from Peter Kahle regarding a Windows Phone 7 version of the library - not currently compilable as need to get a couple more resources from him

---
+++ Libraries/windowsphone/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("dotNetRDF (Windows Phone 7 Build)")]
+[assembly: AssemblyDescription("API for manipulating RDF data (slimmed down Windows Phone 7 build)")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Visual Design Studios")]
+[assembly: AssemblyProduct("dotNetRDF.WindowsPhone")]
+[assembly: AssemblyCopyright("Copyright © Robert Vesse 2009-10 and Peter Kahle 2010-2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9FA6DBEF-1B7D-429c-A1C5-D30EACCC5B69")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.3.1.*")]
+[assembly: AssemblyFileVersion("0.3.1.0")]
