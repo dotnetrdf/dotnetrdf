5cbc631b1caeee1dc2009cf02c5ba26bab647b09
Rob Vesse <rvesse@dotnetrdf.org>
2011-01-25 14:06:40 +0000

    Added an outline project for a rdfPublisher utility which is a GUI Wizard for getting RDF published using the Configuration API and the ASP.Net integration

---
+++ rdfPublisher/fclsPublisher.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace rdfPublisher
+{
+    partial class fclsPublisher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // fclsPublisher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(603, 359);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "fclsPublisher";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "rdfPublisher";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+
+    }
+}
+
