0b5f57978e1b63799cec64d46e1a1e45ae51f5c0
Rob Vesse <rvesse@dotnetrdf.org>
2011-01-26 10:59:30 +0000

    Added a number of additional unit tests to test that other similar scenarios to the GRAPH clause bug just fixed were not issues

--- Testing/unittest/Sparql/ParsingTests.cs
+++ Testing/unittest/Sparql/ParsingTests.cs
@@ -11,17 +11,50 @@ namespace VDS.RDF.Test.Sparql
     [TestClass]
     public class ParsingTests
     {
-        [TestMethod]
-        public void SparqlParsingComplexGraphAfterUnion()
-        {
-            String query = "SELECT * WHERE {{?x ?y ?z} UNION {?z ?y ?x} GRAPH ?g {?x ?y ?z}}";
+        private SparqlQueryParser _parser = new SparqlQueryParser();
 
-            SparqlQueryParser parser = new SparqlQueryParser();
-            SparqlQuery q = parser.ParseFromString(query);
+        private void TestQuery(String query)
+        {
+            SparqlQuery q = this._parser.ParseFromString(query);
 
             Console.WriteLine(q.ToString());
             Console.WriteLine();
             Console.WriteLine(q.ToAlgebra().ToString());
+        }
+
+        [TestMethod]
+        public void SparqlParsingComplexGraphAfterUnion()
+        {
+            String query = "SELECT * WHERE {{?x ?y ?z} UNION {?z ?y ?x} GRAPH ?g {?x ?y ?z}}";
+            this.TestQuery(query);
+        }
+
+        [TestMethod]
+        public void SparqlParsingComplexFilterAfterUnion()
+        {
+            String query = "SELECT * WHERE {{?x ?y ?z} UNION {?z ?y ?x} FILTER(true)}";
+            this.TestQuery(query);
+        }
+
+        [TestMethod]
+        public void SparqlParsingComplexOptionalAfterUnion()
+        {
+            String query = "SELECT * WHERE {{?x ?y ?z} UNION {?z ?y ?x} OPTIONAL {?x a ?u}}";
+            this.TestQuery(query);
+        }
+
+        [TestMethod]
+        public void SparqlParsingComplexMinusAfterUnion()
+        {
+            String query = "SELECT * WHERE {{?x ?y ?z} UNION {?z ?y ?x} MINUS {?s ?p ?o}}";
+            this.TestQuery(query);
+        }
+
+        [TestMethod]
+        public void SparqlParsingComplexOptionalServiceUnion()
+        {
+            String query = "SELECT * WHERE {{?x ?y ?z} UNION {?z ?y ?x} SERVICE ?g {?x ?y ?z}}";
+            this.TestQuery(query);
         }
     }
 }
