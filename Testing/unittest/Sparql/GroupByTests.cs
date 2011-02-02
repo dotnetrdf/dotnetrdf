501f1127edf195ea7885887b7920a2e55e81faaa
Rob Vesse <rvesse@dotnetrdf.org>
2011-02-02 09:05:02 +0000

    Couple more SPARQL related unit tests

--- Testing/unittest/Sparql/GroupByTests.cs
+++ Testing/unittest/Sparql/GroupByTests.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VDS.RDF.Parsing;
 using VDS.RDF.Query;
+using VDS.RDF.Writing.Formatting;
 
 namespace VDS.RDF.Test.Sparql
 {
@@ -17,6 +18,90 @@ namespace VDS.RDF.Test.Sparql
             String query = "SELECT ?s WHERE {{SELECT * WHERE {?s ?p ?o} GROUP BY ?s}} GROUP BY ?s";
             SparqlQueryParser parser = new SparqlQueryParser();
             SparqlQuery q = parser.ParseFromString(query);
+        }
+
+        [TestMethod]
+        public void SparqlGroupByAssignmentSimple()
+        {
+            String query = "SELECT ?x WHERE { ?s ?p ?o } GROUP BY ?s AS ?x";
+            SparqlQueryParser parser = new SparqlQueryParser();
+            SparqlQuery q = parser.ParseFromString(query);
+
+            SparqlFormatter formatter = new SparqlFormatter();
+            Console.WriteLine(formatter.Format(q));
+            Console.WriteLine();
+
+            QueryableGraph g = new QueryableGraph();
+            FileLoader.Load(g, "InferenceTest.ttl");
+
+            Object results = g.ExecuteQuery(q);
+            if (results is SparqlResultSet)
+            {
+                SparqlResultSet rset = (SparqlResultSet)results;
+                TestTools.ShowResults(rset);
+
+                Assert.IsTrue(rset.All(r => r.HasValue("x") && !r.HasValue("s")), "All Results should have a ?x variable and no ?s variable");
+            }
+            else
+            {
+                Assert.Fail("Didn't get a Result Set as expected");
+            }
+        }
+
+        [TestMethod]
+        public void SparqlGroupByAssignmentSimple2()
+        {
+            String query = "SELECT ?x (COUNT(?p) AS ?predicates) WHERE { ?s ?p ?o } GROUP BY ?s AS ?x";
+            SparqlQueryParser parser = new SparqlQueryParser();
+            SparqlQuery q = parser.ParseFromString(query);
+
+            SparqlFormatter formatter = new SparqlFormatter();
+            Console.WriteLine(formatter.Format(q));
+            Console.WriteLine();
+
+            QueryableGraph g = new QueryableGraph();
+            FileLoader.Load(g, "InferenceTest.ttl");
+
+            Object results = g.ExecuteQuery(q);
+            if (results is SparqlResultSet)
+            {
+                SparqlResultSet rset = (SparqlResultSet)results;
+                TestTools.ShowResults(rset);
+
+                Assert.IsTrue(rset.All(r => r.HasValue("x") && !r.HasValue("s") && r.HasValue("predicates")), "All Results should have a ?x and ?predicates variables and no ?s variable");
+            }
+            else
+            {
+                Assert.Fail("Didn't get a Result Set as expected");
+            }
+        }
+
+        [TestMethod]
+        public void SparqlGroupByAssignmentExpression()
+        {
+            String query = "SELECT ?s ?sum WHERE { ?s ?p ?o } GROUP BY ?s (1 + 2 AS ?sum)";
+            SparqlQueryParser parser = new SparqlQueryParser();
+            SparqlQuery q = parser.ParseFromString(query);
+
+            SparqlFormatter formatter = new SparqlFormatter();
+            Console.WriteLine(formatter.Format(q));
+            Console.WriteLine();
+
+            QueryableGraph g = new QueryableGraph();
+            FileLoader.Load(g, "InferenceTest.ttl");
+
+            Object results = g.ExecuteQuery(q);
+            if (results is SparqlResultSet)
+            {
+                SparqlResultSet rset = (SparqlResultSet)results;
+                TestTools.ShowResults(rset);
+
+                Assert.IsTrue(rset.All(r => r.HasValue("s") && r.HasValue("sum")), "All Results should have a ?s and a ?sum variable");
+            }
+            else
+            {
+                Assert.Fail("Didn't get a Result Set as expected");
+            }
         }
     }
 }
--- Testing/unittest/Sparql/SparqlParsingComplex.cs
+++ Testing/unittest/Sparql/SparqlParsingComplex.cs
@@ -46,5 +46,29 @@ namespace VDS.RDF.Test.Sparql
                 TestTools.ReportError("Parsing Error", parseEx, true);
             }
         }
+
+        [TestMethod]
+        public void SparqlSubQueryWithLimitAndOrderBy()
+        {
+            Graph g = new Graph();
+            FileLoader.Load(g, "InferenceTest.ttl");
+
+            String query = "SELECT * WHERE { { SELECT * WHERE {?s ?p ?o} ORDER BY ?p ?o LIMIT 2 } }";
+            SparqlQueryParser parser = new SparqlQueryParser();
+            SparqlQuery q = parser.ParseFromString(query);
+
+            Object results = g.ExecuteQuery(q);
+            if (results is SparqlResultSet)
+            {
+                SparqlResultSet rset = (SparqlResultSet)results;
+                TestTools.ShowResults(rset);
+
+                Assert.IsTrue(rset.All(r => r.HasValue("s") && r.HasValue("p") && r.HasValue("o")), "All Results should have had ?s, ?p and ?o variables");
+            }
+            else
+            {
+                Assert.Fail("Expected a SPARQL Result Set");
+            }
+        }
     }
 }
