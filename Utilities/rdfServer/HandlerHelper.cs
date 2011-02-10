29272406426cdeb5e9f4b60a56f0287bffa6ae48
Rob Vesse <rvesse@dotnetrdf.org>
2011-02-10 15:10:24 +0000

    Moved rdfServer to the experimental branch as not ready for public release

---
+++ Utilities/rdfServer/HandlerHelper.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using VDS.RDF.Web.Configuration;
+using VDS.Web;
+
+namespace rdfServer
+{
+    public static class HandlerHelper
+    {
+        /// <summary>
+        /// Handles errors in processing SPARQL Query Requests
+        /// </summary>
+        /// <param name="context">Context of the HTTP Request</param>
+        /// <param name="config">Handler Configuration</param>
+        /// <param name="title">Error title</param>
+        /// <param name="query">Sparql Query</param>
+        /// <param name="ex">Error</param>
+        public static void HandleQueryErrors(HttpServerContext context, BaseHandlerConfiguration config, String title, String query, Exception ex)
+        {
+            HandleQueryErrors(context, config, title, query, ex, (int)HttpStatusCode.InternalServerError);
+        }
+
+        /// <summary>
+        /// Handles errors in processing SPARQL Query Requests
+        /// </summary>
+        /// <param name="context">Context of the HTTP Request</param>
+        /// <param name="config">Handler Configuration</param>
+        /// <param name="title">Error title</param>
+        /// <param name="query">Sparql Query</param>
+        /// <param name="ex">Error</param>
+        /// <param name="statusCode">HTTP Status Code to return</param>
+        public static void HandleQueryErrors(HttpServerContext context, BaseHandlerConfiguration config, String title, String query, Exception ex, int statusCode)
+        {
+            //Clear any existing Response and set our HTTP Status Code
+            context.Response.StatusCode = statusCode;
+
+            if (config != null)
+            {
+                //If not showing errors then we won't return our custom error description
+                if (!config.ShowErrors) return;
+            }
+
+            //Set to Plain Text output and report the error
+            context.Response.ContentEncoding = System.Text.Encoding.UTF8;
+            context.Response.ContentType = "text/plain";
+
+            //Error Title
+            using (StreamWriter writer = new StreamWriter(context.Response.OutputStream))
+            {
+                writer.Write(title + "\n");
+                writer.Write(new String('-', title.Length) + "\n\n");
+
+                //Output Query with Line Numbers
+                if (query != null && !query.Equals(String.Empty))
+                {
+                    String[] lines = query.Split('\n');
+                    for (int l = 0; l < lines.Length; l++)
+                    {
+                        writer.Write((l + 1) + ": " + lines[l] + "\n");
+                    }
+                    writer.Write("\n\n");
+                }
+
+                //Error Message
+                writer.Write(ex.Message + "\n");
+
+#if DEBUG
+                //Stack Trace only when Debug build
+                writer.Write(ex.StackTrace + "\n\n");
+                while (ex.InnerException != null)
+                {
+                    ex = ex.InnerException;
+                    writer.Write(ex.Message + "\n");
+                    writer.Write(ex.StackTrace + "\n\n");
+                }
+#endif
+                writer.Close();
+            }
+        }
+
+        /// <summary>
+        /// Handles errors in processing SPARQL Update Requests
+        /// </summary>
+        /// <param name="context">Context of the HTTP Request</param>
+        /// <param name="config">Handler Configuration</param>
+        /// <param name="title">Error title</param>
+        /// <param name="update">SPARQL Update</param>
+        /// <param name="ex">Error</param>
+        public static void HandleUpdateErrors(HttpServerContext context, BaseHandlerConfiguration config, String title, String update, Exception ex)
+        {
+            HandleUpdateErrors(context, config, title, update, ex, (int)HttpStatusCode.InternalServerError);
+        }
+
+        /// <summary>
+        /// Handles errors in processing SPARQL Update Requests
+        /// </summary>
+        /// <param name="context">Context of the HTTP Request</param>
+        /// <param name="config">Handler Configuration</param>
+        /// <param name="title">Error title</param>
+        /// <param name="update">SPARQL Update</param>
+        /// <param name="ex">Error</param>
+        /// <param name="statusCode">HTTP Status Code to return</param>
+        public static void HandleUpdateErrors(HttpServerContext context, BaseHandlerConfiguration config, String title, String update, Exception ex, int statusCode)
+        {
+            if (config != null)
+            {
+                if (!config.ShowErrors)
+                {
+                    context.Response.StatusCode = statusCode;
+                    return;
+                }
+            }
+
+            //Set to Plain Text output and report the error
+            context.Response.ContentEncoding = System.Text.Encoding.UTF8;
+            context.Response.ContentType = "text/plain";
+
+            //Error Title
+            using (StreamWriter writer = new StreamWriter(context.Response.OutputStream))
+            {
+                writer.Write(title + "\n");
+                writer.Write(new String('-', title.Length) + "\n\n");
+
+                //Output Query with Line Numbers
+                if (update != null && !update.Equals(String.Empty))
+                {
+                    String[] lines = update.Split('\n');
+                    for (int l = 0; l < lines.Length; l++)
+                    {
+                        writer.Write((l + 1) + ": " + lines[l] + "\n");
+                    }
+                    writer.Write("\n\n");
+                }
+
+                //Error Message
+                writer.Write(ex.Message + "\n");
+
+#if DEBUG
+                //Stack Trace only when Debug build
+                writer.Write(ex.StackTrace + "\n\n");
+                while (ex.InnerException != null)
+                {
+                    ex = ex.InnerException;
+                    writer.Write(ex.Message + "\n");
+                    writer.Write(ex.StackTrace + "\n\n");
+                }
+#endif
+                writer.Close();
+            }
+        }
+
+    }
+}
---
+++ Utilities/rdfServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("rdfServer")]
+[assembly: AssemblyDescription("A simple SPARQL Query and Update server accessible over HTTP")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Visual Design Studios")]
+[assembly: AssemblyProduct("rdfServer")]
+[assembly: AssemblyCopyright("Copyright © Robert Vesse 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9ef2dd7b-9acd-40e0-b79c-3e095dd63d3b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
