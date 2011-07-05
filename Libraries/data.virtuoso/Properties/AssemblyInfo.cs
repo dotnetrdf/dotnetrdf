45e28f2ec2adae82ed180f01f334bbca662902b8
Rob Vesse <rvesse@dotnetrdf.org>
2011-07-05 13:34:35 +0000

    Added a new project to split the virtuoso support out from the core library

--- Libraries/data.virtuoso/Properties/AssemblyInfo.cs
+++ Libraries/data.virtuoso/Properties/AssemblyInfo.cs
@@ -5,22 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("dotNetRDF.Data.Sql")]
-[assembly: AssemblyDescription("SQL Backend for dotNetRDF")]
+[assembly: AssemblyTitle("dotNetRDF.Data.Virtuoso")]
+[assembly: AssemblyDescription("Virtuoso Backend for dotNetRDF")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Visual Design Studios")]
-[assembly: AssemblyProduct("dotNetRDF.Data.Sql")]
+[assembly: AssemblyProduct("dotNetRDF.Data.Virtuoso")]
 [assembly: AssemblyCopyright("Copyright © Robert Vesse 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
-// to COM componenets.  If you need to access a type in this assembly from 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9e32fd0f-5f21-42a9-ab2b-ab1c5e4dec83")]
+[assembly: Guid("ab4c87e7-5b5b-492c-9db3-a01a2a086c97")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.5.0.*")]
 [assembly: AssemblyFileVersion("0.5.0.0")]
