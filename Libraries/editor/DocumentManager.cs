a5583a77bb7bc5a6b8b0ad055de70ed918fdb31c
Rob Vesse <rvesse@dotnetrdf.org>
2011-07-14 13:38:45 +0000

    Created an rdfEditor.Core.dll library which will serve as the core of a major refactoring of rdfEditor which will allow it to be ported to WinForms as well as WPF in the future and support multi-document editing in a single window

---
+++ Libraries/editor/DocumentManager.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VDS.RDF.Utilities.Editor
+{
+    public class DocumentManager
+    {
+        private List<Document> _documents = new List<Document>();
+        private int _current = 0;
+        private GlobalOptions _options = new GlobalOptions();
+
+        public GlobalOptions Options
+        {
+            get
+            {
+                return this._options;
+            }
+        }
+
+        public Document Current
+        {
+            get
+            {
+                if (this._documents.Count > 0)
+                {
+                    return this._documents[this._current];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public Document this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < this._documents.Count)
+                {
+                    return this._documents[index];
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+        public IEnumerable<Document> Documents
+        {
+            get
+            {
+                return this._documents;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this._documents.Count;
+            }
+        }
+
+        private void CorrectIndex()
+        {
+            if (this._current > this._documents.Count && this._documents.Count > 0)
+            {
+                this._current = this._documents.Count - 1;
+            }
+        }
+
+        public void Add(Document doc)
+        {
+            this.Add(doc, false);
+        }
+
+        public void Add(Document doc, bool switchTo)
+        {
+            this._documents.Add(doc);
+            if (switchTo)
+            {
+                this._current = this._documents.Count - 1;
+            }
+        }
+
+        public void Close()
+        {
+            if (this._documents.Count > 0)
+            {
+                this._documents.RemoveAt(this._current);
+                this.CorrectIndex();
+            }
+        }
+
+        public void Close(int index)
+        {
+            if (index >= 0 && index < this._documents.Count)
+            {
+                this._documents.RemoveAt(index);
+                this.CorrectIndex();
+            }
+        }
+
+        public void CloseAll()
+        {
+            this._documents.Clear();
+        }
+
+        public void SaveAll()
+        {
+
+        }
+    }
+}
---
+++ Libraries/editor/GlobalOptions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VDS.RDF.Utilities.Editor
+{
+    public class GlobalOptions
+    {
+        //Selection
+        private bool _symbolSelectEnabled = true;
+        private ISymbolSelector _selector = new DefaultSelector();
+        private bool _includeDelim = false;
+
+        //Validation
+        private bool _validateAsYouType = false;
+        private bool _highlightErrors = true;
+
+        /// <summary>
+        /// Gets/Sets whether Symbol Selection is enabled
+        /// </summary>
+        public bool IsSymbolSelectionEnabled
+        {
+            get
+            {
+                return this._symbolSelectEnabled;
+            }
+            set
+            {
+                this._symbolSelectEnabled = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets/Sets whether Symbol Boundaries are included when using Symbol Selection
+        /// </summary>
+        public bool IncludeBoundaryInSymbolSelection
+        {
+            get
+            {
+                if (this._selector != null)
+                {
+                    return this._selector.IncludeDeliminator;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            set
+            {
+                this._includeDelim = value;
+                if (this._selector != null)
+                {
+                    this._selector.IncludeDeliminator = value;
+                }
+            }
+        }
+    }
+}
