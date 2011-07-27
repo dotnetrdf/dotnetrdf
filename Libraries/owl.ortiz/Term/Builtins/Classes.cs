3370019b232900aef78050896ec162d2d84ff220
Rob Vesse <rvesse@dotnetrdf.org>
2011-07-27 13:21:42 +0000

    More OWL implementation stubs

---
+++ Libraries/owl.ortiz/Term/Builtins/Classes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VDS.OWL.Term.Entity;
+
+namespace VDS.OWL.Term.Builtins
+{
+    public static class Classes
+    {
+        public static INamedClass Bottom
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public static INamedClass Top
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
