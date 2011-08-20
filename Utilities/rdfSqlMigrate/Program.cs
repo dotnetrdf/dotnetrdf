ca7877e602b33fa6eb1b06c3abab89a866c295ee
Rob Vesse <rvesse@dotnetrdf.org>
2011-08-20 12:28:44 +0000

    Added stub for SQL Migration tool Affects Issue [70]

---
+++ Utilities/rdfSqlMigrate/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VDS.RDF.Utilities.Data.Sql.Migrate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                ShowUsage();
+            }
+            else
+            {
+                Console.Error.WriteLine("Not Yet Implemented!");
+            }
+        }
+
+        private static void ShowUsage()
+        {
+            Console.WriteLine("rdfSqlMigrate");
+            Console.WriteLine("=============");
+            Console.WriteLine();
+            Console.WriteLine("Migrates from the legacy dotNetRDF SQL store format to the new ADO store format that replaces it");
+            Console.WriteLine();
+            Console.WriteLine("Usage is rdfSqlMigrate MigrateConfig.ttl [options]");
+            Console.WriteLine();
+            Console.WriteLine("Please see the included SampleMigrationConfig.ttl file for an example migration configuration file");
+            Console.WriteLine();
+            Console.WriteLine("Supported Options");
+            Console.WriteLine("-----------------");
+            Console.WriteLine();
+            Console.WriteLine("-verify");
+            Console.WriteLine(" Verifies the data after it has been migrated");
+        }
+    }
+}
---
+++ Utilities/rdfSqlMigrate/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("rdfSqlMigrate")]
+[assembly: AssemblyDescription("A tool for migrating the legacy dotNetRDF SQL store format to the newer ADO store format")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Visual Design Studios")]
+[assembly: AssemblyProduct("rdfSqlMigrate")]
+[assembly: AssemblyCopyright("Copyright © Robert Vesse 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f660c5db-f15d-453a-be99-42a326e1a7a2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
