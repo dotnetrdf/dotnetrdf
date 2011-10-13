ed9b9f4c21b98922a1198e4dafc745fd6d90b232
Rob Vesse <rvesse@dotnetrdf.org>
2011-10-13 09:36:10 +0000

    Some bug fixes to dormant Alexandria stuff in order to test it out just as a plugin to Store Manager to check the plugin mechanism works correctly

---
+++ Libraries/storemanager.plugin.mongodb/Connections/MongoDBConnectionDefinition.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using VDS.Alexandria;
+using VDS.RDF.Storage;
+
+namespace VDS.RDF.Utilities.StoreManager.Connections
+{
+    public class MongoDBConnectionDefinition
+        : BaseConnectionDefinition
+    {
+        public MongoDBConnectionDefinition()
+            : base("Alexandria MongoDB", "Use our experimental Alexandria document based Triple Store with MongoDB as the underlying store.  To connect to a MongoDB server on the local machine with standard settings just enter a DB Name, otherwise tick the Use Custom Connection String button and enter a MongoDB Connection String.") { }
+
+        [Connection(DisplayName="Database", DisplayOrder = 1, IsRequired=true)]
+        public String Database
+        {
+            get;
+            set;
+        }
+
+        [Connection(DisplayName="Use Custom Connection String?", DisplayOrder=2, Type=ConnectionSettingType.Boolean),
+         DefaultValue(false)]
+        public bool UseCustomConnectionString
+        {
+            get;
+            set;
+        }
+
+        [Connection(DisplayName="Connection String", DisplayOrder=3),
+         DefaultValue("mongodb://localhost")]
+        public String ConnectionString
+        {
+            get;
+            set;
+        }
+
+        protected override IGenericIOManager OpenConnectionInternal()
+        {
+            if (this.UseCustomConnectionString)
+            {
+                return new AlexandriaMongoDBManager(this.ConnectionString, this.Database);
+            }
+            else
+            {
+                return new AlexandriaMongoDBManager(this.Database);
+            }
+        }
+    }
+}
--- Testing/alexandria-tests/GraphRegistryTests.cs
+++ Testing/alexandria-tests/GraphRegistryTests.cs
@@ -384,6 +384,241 @@ namespace alexandria_tests
             manager.Dispose();
         }
 
+        [TestMethod]
+        public void MongoTCGraphRegistryEnumerate()
+        {
+            //Load in our Test Graph
+            Graph g = new Graph();
+            FileLoader.Load(g, "InferenceTest.ttl");
+
+            //Open an Alexandria Store and save the Graph
+            AlexandriaMongoDBManager manager = new AlexandriaMongoDBManager(TestTools.GetNextStoreID(), MongoDBSchemas.TripleCentric);
+            manager.SaveGraph(g);
+
+            //Now load another Graph and add it
+            Graph i = new Graph();
+            FileLoader.Load(i, "test.nt");
+
+            //Save it to the Store which should overwrite it and then load it back
+            manager.SaveGraph(i);
+
+            //Now enumerate the Graph URIs and Document Names
+            TestWrapper<Document, Document> wrapper = new TestWrapper<Document, Document>(manager);
+            IGraphRegistry registry = wrapper.DocumentManager.GraphRegistry;
+            Console.WriteLine("Graph URIs");
+            foreach (String uri in registry.GraphUris)
+            {
+                Console.WriteLine(uri);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Document Names");
+            foreach (String name in registry.DocumentNames)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Document -> Graph Mappings");
+            foreach (KeyValuePair<String, String> kvp in registry.DocumentToGraphMappings)
+            {
+                Console.WriteLine(kvp.Key + " -> " + kvp.Value);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Graph -> Document Mappins");
+            foreach (KeyValuePair<String, String> kvp in registry.GraphToDocumentMappings)
+            {
+                Console.WriteLine(kvp.Key + " -> " + kvp.Value);
+            }
+            Console.WriteLine();
+
+            manager.Dispose();
+        }
+
+        [TestMethod]
+        public void MongoTCGraphRegistryEnumerateWithDefaultGraph()
+        {
+            //Load in our Test Graph
+            Graph g = new Graph();
+            FileLoader.Load(g, "InferenceTest.ttl");
+            g.BaseUri = null;
+
+            //Open an Alexandria Store and save the Graph
+            AlexandriaMongoDBManager manager = new AlexandriaMongoDBManager(TestTools.GetNextStoreID(), MongoDBSchemas.TripleCentric);
+            manager.SaveGraph(g);
+
+            //Now load another Graph and add it
+            Graph i = new Graph();
+            FileLoader.Load(i, "test.nt");
+
+            //Save it to the Store which should overwrite it and then load it back
+            manager.SaveGraph(i);
+
+            //Now enumerate the Graph URIs and Document Names
+            TestWrapper<Document, Document> wrapper = new TestWrapper<Document, Document>(manager);
+            IGraphRegistry registry = wrapper.DocumentManager.GraphRegistry;
+            Console.WriteLine("Graph URIs");
+            foreach (String uri in registry.GraphUris)
+            {
+                Console.WriteLine(uri);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Graph URIs via ListGraphs");
+            foreach (Uri u in manager.ListGraphs())
+            {
+                Console.WriteLine(u != null ? u.ToString() : "Default Graph");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Document Names");
+            foreach (String name in registry.DocumentNames)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Document -> Graph Mappings");
+            foreach (KeyValuePair<String, String> kvp in registry.DocumentToGraphMappings)
+            {
+                Console.WriteLine(kvp.Key + " -> " + kvp.Value);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Graph -> Document Mappins");
+            foreach (KeyValuePair<String, String> kvp in registry.GraphToDocumentMappings)
+            {
+                Console.WriteLine(kvp.Key + " -> " + kvp.Value);
+            }
+            Console.WriteLine();
+
+            manager.Dispose();
+        }
+
+        [TestMethod]
+        public void MongoTCGraphRegistryEnumerateWithDefaultGraphOnly()
+        {
+            //Load in our Test Graph
+            Graph g = new Graph();
+            FileLoader.Load(g, "InferenceTest.ttl");
+            g.BaseUri = null;
+
+            //Open an Alexandria Store and save the Graph
+            AlexandriaMongoDBManager manager = new AlexandriaMongoDBManager(TestTools.GetNextStoreID(), MongoDBSchemas.TripleCentric);
+            manager.SaveGraph(g);
+
+            //Now enumerate the Graph URIs and Document Names
+            TestWrapper<Document, Document> wrapper = new TestWrapper<Document, Document>(manager);
+            IGraphRegistry registry = wrapper.DocumentManager.GraphRegistry;
+            Console.WriteLine("Graph URIs");
+            foreach (String uri in registry.GraphUris)
+            {
+                Console.WriteLine(uri);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Graph URIs via ListGraphs");
+            foreach (Uri u in manager.ListGraphs())
+            {
+                Console.WriteLine(u != null ? u.ToString() : "Default Graph");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Document Names");
+            foreach (String name in registry.DocumentNames)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Document -> Graph Mappings");
+            foreach (KeyValuePair<String, String> kvp in registry.DocumentToGraphMappings)
+            {
+                Console.WriteLine(kvp.Key + " -> " + kvp.Value);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Graph -> Document Mappins");
+            foreach (KeyValuePair<String, String> kvp in registry.GraphToDocumentMappings)
+            {
+                Console.WriteLine(kvp.Key + " -> " + kvp.Value);
+            }
+            Console.WriteLine();
+
+            manager.Dispose();
+        }
+
+        [TestMethod]
+        public void MongoTCGraphRegistryLookup()
+        {
+            //Load in our Test Graph
+            Graph g = new Graph();
+            FileLoader.Load(g, "InferenceTest.ttl");
+            g.BaseUri = null;
+
+            //Open an Alexandria Store and save the Graph
+            AlexandriaMongoDBManager manager = new AlexandriaMongoDBManager(TestTools.GetNextStoreID(), MongoDBSchemas.TripleCentric);
+            manager.SaveGraph(g);
+
+            TestWrapper<Document, Document> wrapper = new TestWrapper<Document, Document>(manager);
+            String graphUri = (g.BaseUri != null) ? g.BaseUri.ToString() : String.Empty;
+            String name = wrapper.DocumentManager.GraphRegistry.GetDocumentName(graphUri);
+
+            String retrievedDocumentName = wrapper.DocumentManager.GraphRegistry.GetDocumentName(graphUri);
+            Assert.AreEqual(name, retrievedDocumentName, "Document Names should have been equal");
+            String retrievedGraphUri = wrapper.DocumentManager.GraphRegistry.GetGraphUri(name);
+            Assert.AreEqual(graphUri, retrievedGraphUri, "Graph URIs should have been equal");
+
+            manager.Dispose();
+        }
+
+        [TestMethod]
+        public void MongoTCGraphRegisterUnregister()
+        {
+            //Load in our Test Graph
+            Graph g = new Graph();
+            FileLoader.Load(g, "InferenceTest.ttl");
+            g.BaseUri = null;
+
+            //Open an Alexandria Store and save the Graph
+            AlexandriaMongoDBManager manager = new AlexandriaMongoDBManager(TestTools.GetNextStoreID(), MongoDBSchemas.TripleCentric);
+            manager.SaveGraph(g);
+
+            //Now load another Graph and add it
+            Graph i = new Graph();
+            FileLoader.Load(i, "test.nt");
+
+            //Save it to the Store which should overwrite it and then load it back
+            manager.SaveGraph(i);
+
+            //Now enumerate the Graph URIs and Document Names
+            TestWrapper<Document, Document> wrapper = new TestWrapper<Document, Document>(manager);
+            IGraphRegistry registry = wrapper.DocumentManager.GraphRegistry;
+            Console.WriteLine("Graph URIs");
+            foreach (String uri in registry.GraphUris)
+            {
+                Console.WriteLine(uri);
+            }
+            Assert.IsTrue(registry.GraphUris.Count() == 2, "Should have been 2 Graph URIs");
+            Console.WriteLine();
+            Console.WriteLine("Document Names");
+            foreach (String name in registry.DocumentNames)
+            {
+                Console.WriteLine(name);
+            }
+            Assert.IsTrue(registry.DocumentNames.Count() == 2, "Should have been 2 Document Names");
+
+            //Now delete one of the Graphs
+            manager.DeleteGraph(String.Empty);
+
+            //Again enumerate the Graph URIs and Document Names
+            Console.WriteLine("Graph URIs");
+            foreach (String uri in registry.GraphUris)
+            {
+                Console.WriteLine(uri);
+            }
+            Assert.IsTrue(registry.GraphUris.Count() == 1, "Should have been only 1 Graph URIs");
+            Console.WriteLine();
+            Console.WriteLine("Document Names");
+            foreach (String name in registry.DocumentNames)
+            {
+                Console.WriteLine(name);
+            }
+            Assert.IsTrue(registry.DocumentNames.Count() == 1, "Should have been only 1 Document Names");
+
+            manager.Dispose();
+        }
+
         #endregion
     }
 }
