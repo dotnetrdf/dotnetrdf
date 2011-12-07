ed852e838e9376b20c552f72697a4f59145c5076
Rob Vesse <rvesse@dotnetrdf.org>
2011-12-07 18:17:58 +0000

    Moved interop.intellidimension to obsolete branch as never implemnted anything on this and no user ever asked for this

---
+++ Libraries/interop.intellidimension/Intellidimension/IntellidimensionConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VDS.RDF.Interop
+{
+    //TODO: Implement interoperability with Intellidimension's Semantics SDK (if licensing allows this)
+
+    class IntellidimensionConverter
+    {
+    }
+}
