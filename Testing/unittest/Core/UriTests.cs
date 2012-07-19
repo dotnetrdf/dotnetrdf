b70b409350d7d847ba1faebdb9fa3d6851334408
Rob Vesse <rvesse@dotnetrdf.org>
2012-07-19 17:05:18 +0000

    Added a couple of new tests around CORE-258 to ensure that the change to using AbsoluteUri does not interfere with behavior involving query string and fragments Affects Issue [258]

---
+++ Testing/unittest/Core/UriTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VDS.RDF.Test.Core
+{
+    [TestClass]
+    public class UriTests
+    {
+        [TestMethod]
+        public void UriAbsoluteUriWithQuerystring()
+        {
+            Uri u = new Uri("http://example.org/?test");
+            Assert.AreEqual("http://example.org/?test", u.AbsoluteUri);
+        }
+
+        [TestMethod]
+        public void UriAbsoluteUriWithFragment()
+        {
+            Uri u = new Uri("http://example.org/#test");
+            Assert.AreEqual("http://example.org/#test", u.AbsoluteUri);
+        }
+    }
+}
