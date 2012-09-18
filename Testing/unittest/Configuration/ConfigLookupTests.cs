73d5e74a25be8e0534149f9d35189301177325de
Rob Vesse <rvesse@dotnetrdf.org>
2012-09-18 11:24:40 -0700

    Fleshes out unit tests for ConfigurationLoader methods more (CORE-270)

--- Testing/unittest/Configuration/ConfigLookupTests.cs
+++ Testing/unittest/Configuration/ConfigLookupTests.cs
@@ -121,5 +121,102 @@ _:a dnr:type ""not a boolean"" .";
             bool value = ConfigurationLoader.GetConfigurationBoolean(g, g.GetBlankNode("a"), g.CreateUriNode("dnr:type"), false);
             Assert.IsFalse(value);
         }
+
+        [TestMethod]
+        public void ConfigurationLookupBoolean5()
+        {
+            String graph = Prefixes + @"
+_:a dnr:type <appsetting:ConfigurationLookupBoolean5> .";
+
+            System.Configuration.ConfigurationManager.AppSettings["ConfigurationLookupBoolean5"] = "true";
+
+            Graph g = new Graph();
+            g.LoadFromString(graph);
+
+            bool value = ConfigurationLoader.GetConfigurationBoolean(g, g.GetBlankNode("a"), g.CreateUriNode("dnr:type"), false);
+            Assert.IsTrue(value);
+        }
+
+        [TestMethod]
+        public void ConfigurationLookupInt1()
+        {
+            String graph = Prefixes + @"
+_:a dnr:type 123 .";
+
+            Graph g = new Graph();
+            g.LoadFromString(graph);
+
+            int value = ConfigurationLoader.GetConfigurationInt32(g, g.GetBlankNode("a"), g.CreateUriNode("dnr:type"), 0);
+            Assert.AreEqual(123, value);
+        }
+
+        [TestMethod]
+        public void ConfigurationLookupInt2()
+        {
+            String graph = Prefixes + @"
+_:a dnr:type ""not an integer"" .";
+
+            Graph g = new Graph();
+            g.LoadFromString(graph);
+
+            int value = ConfigurationLoader.GetConfigurationInt32(g, g.GetBlankNode("a"), g.CreateUriNode("dnr:type"), 0);
+            Assert.AreEqual(0, value);
+        }
+
+        [TestMethod]
+        public void ConfigurationLookupInt3()
+        {
+            String graph = Prefixes + @"
+_:a dnr:type <appsetting:ConfigurationLookupInt3> .";
+
+            System.Configuration.ConfigurationManager.AppSettings["ConfigurationLookupInt3"] = "123";
+
+            Graph g = new Graph();
+            g.LoadFromString(graph);
+
+            int value = ConfigurationLoader.GetConfigurationInt32(g, g.GetBlankNode("a"), g.CreateUriNode("dnr:type"), 0);
+            Assert.AreEqual(123, value);
+        }
+
+        [TestMethod]
+        public void ConfigurationLookupLong1()
+        {
+            String graph = Prefixes + @"
+_:a dnr:type 123 .";
+
+            Graph g = new Graph();
+            g.LoadFromString(graph);
+
+            long value = ConfigurationLoader.GetConfigurationInt64(g, g.GetBlankNode("a"), g.CreateUriNode("dnr:type"), 0);
+            Assert.AreEqual(123, value);
+        }
+
+        [TestMethod]
+        public void ConfigurationLookupLong2()
+        {
+            String graph = Prefixes + @"
+_:a dnr:type ""not an integer"" .";
+
+            Graph g = new Graph();
+            g.LoadFromString(graph);
+
+            long value = ConfigurationLoader.GetConfigurationInt64(g, g.GetBlankNode("a"), g.CreateUriNode("dnr:type"), 0);
+            Assert.AreEqual(0, value);
+        }
+
+        [TestMethod]
+        public void ConfigurationLookupLong3()
+        {
+            String graph = Prefixes + @"
+_:a dnr:type <appsetting:ConfigurationLookupLong3> .";
+
+            System.Configuration.ConfigurationManager.AppSettings["ConfigurationLookupLong3"] = "123";
+
+            Graph g = new Graph();
+            g.LoadFromString(graph);
+
+            long value = ConfigurationLoader.GetConfigurationInt32(g, g.GetBlankNode("a"), g.CreateUriNode("dnr:type"), 0);
+            Assert.AreEqual(123, value);
+        }
     }
 }
