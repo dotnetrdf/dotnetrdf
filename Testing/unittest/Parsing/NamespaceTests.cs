e71a01a212db7991552cc18f1eb6e3f61266712d
Rob Vesse <rvesse@dotnetrdf.org>
2012-09-24 10:37:23 -0700

    Adds unit tests to reproduce CORE-280 as reported by Uldis Bojars

---
+++ Testing/unittest/Parsing/NamespaceTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VDS.RDF.Parsing;
+
+namespace VDS.RDF.Test.Parsing
+{
+    [TestClass]
+    public class NamespaceTests
+    {
+        [TestMethod, ExpectedException(typeof(RdfParseException))]
+        public void ParsingNamespaceNotDeclaredGraphInvokeRdfXml()
+        {
+            Graph g = new Graph();
+            RdfXmlParser parser = new RdfXmlParser();
+            g.LoadFromFile("rdfxml-namespace-declaration.rdf", parser);
+        }
+
+        [TestMethod, ExpectedException(typeof(RdfParseException))]
+        public void ParsingNamespaceNotDeclaredParserInvokeRdfXml()
+        {
+            Graph g = new Graph();
+            RdfXmlParser parser = new RdfXmlParser();
+            parser.Load(g, "rdfxml-namespace-declaration.rdf");
+        }
+    }
+}
