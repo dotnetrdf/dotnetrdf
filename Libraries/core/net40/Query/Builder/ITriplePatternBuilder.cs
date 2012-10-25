e64eaafdffb74d931afdfafb9bc6f4dd14368bc1
Tomasz Pluskiewicz <tomasz@t-code.pl>
2012-10-25 21:04:39 +0200

    added missing ITriplePatternBuilder method

--- Libraries/core/net40/Query/Builder/ITriplePatternBuilder.cs
+++ Libraries/core/net40/Query/Builder/ITriplePatternBuilder.cs
@@ -1,9 +1,12 @@
-﻿namespace VDS.RDF.Query.Builder
+﻿using System;
+
+namespace VDS.RDF.Query.Builder
 {
     public interface ITriplePatternBuilder
     {
         TriplePatternPredicatePart Subject(string subjectVariableName);
         TriplePatternPredicatePart Subject<TNode>(string subject) where TNode : INode;
         TriplePatternPredicatePart Subject(INode subjectNode);
+        TriplePatternPredicatePart Subject(Uri subject);
     }
 }
