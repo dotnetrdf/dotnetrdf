badfedd47638bbcdca9940cbb2569b3773500763
Tomasz Pluskiewicz <tomasz@t-code.pl>
2012-10-30 07:31:21 +0100

    expression builder tests started

--- Testing/unittest/Builder/ExpressionBuilderTests.cs
+++ Testing/unittest/Builder/ExpressionBuilderTests.cs
@@ -1,7 +1,24 @@
-﻿namespace VDS.RDF.Test.Builder
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VDS.RDF.Query.Builder;
+
+namespace VDS.RDF.Test.Builder
 {
+    [TestClass]
     public class ExpressionBuilderTests
     {
-         
+        private ExpressionBuilder _builder;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _builder = new ExpressionBuilder();
+        }
+
+        [TestMethod]
+        public void CanCreateRegexExpression()
+        {
+            // when
+            _builder.Regex("");
+        }
     }
 }
