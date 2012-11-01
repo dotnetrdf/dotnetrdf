596434c46460952a47a914c509288f1a110870bc
Tomasz Pluskiewicz <tomasz@t-code.pl>
2012-11-01 17:11:35 +0100

    move equality methods to SparqlExpression class

--- Libraries/core/net40/Query/Builder/Expressions/SparqlExpression.cs
+++ Libraries/core/net40/Query/Builder/Expressions/SparqlExpression.cs
@@ -19,5 +19,41 @@ namespace VDS.RDF.Query.Builder.Expressions
             var inFunction = new InFunction(Expression, expressions.Select(v => v.Expression));
             return new BooleanExpression(inFunction);
         }
+
+        public BooleanExpression Eq(SparqlExpression rightExpression)
+        {
+            ISparqlExpression equalsExpression = new EqualsExpression(Expression, rightExpression.Expression);
+            return new BooleanExpression(equalsExpression);
+        }
+
+        public BooleanExpression Gt(SparqlExpression rightExpression)
+        {
+            ISparqlExpression equalsExpression = new GreaterThanExpression(Expression, rightExpression.Expression);
+            return new BooleanExpression(equalsExpression);
+        }
+
+        public BooleanExpression Gt(RdfTermExpression rightExpression)
+        {
+            ISparqlExpression equalsExpression = new GreaterThanExpression(Expression, rightExpression.Expression);
+            return new BooleanExpression(equalsExpression);
+        }
+
+        public BooleanExpression Lt(SparqlExpression rightExpression)
+        {
+            ISparqlExpression equalsExpression = new LessThanExpression(Expression, rightExpression.Expression);
+            return new BooleanExpression(equalsExpression);
+        }
+
+        public BooleanExpression Ge(SparqlExpression rightExpression)
+        {
+            ISparqlExpression equalsExpression = new GreaterThanOrEqualToExpression(Expression, rightExpression.Expression);
+            return new BooleanExpression(equalsExpression);
+        }
+
+        public BooleanExpression Le(SparqlExpression rightExpression)
+        {
+            ISparqlExpression equalsExpression = new LessThanOrEqualToExpression(Expression, rightExpression.Expression);
+            return new BooleanExpression(equalsExpression);
+        }
     }
 }
