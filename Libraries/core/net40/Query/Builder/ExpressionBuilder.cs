73e3454df55148f40d9d71e73630fe403d9e9770
Tomasz Pluskiewicz <tomasz@t-code.pl>
2012-11-01 17:29:02 +0100

    cleaning ExpressionBuilder

--- Libraries/core/net40/Query/Builder/ExpressionBuilder.cs
+++ Libraries/core/net40/Query/Builder/ExpressionBuilder.cs
@@ -1,22 +1,10 @@
-using VDS.RDF.Nodes;
 using VDS.RDF.Query.Builder.Expressions;
 using VDS.RDF.Query.Expressions.Conditional;
-using VDS.RDF.Query.Expressions.Functions.Sparql.String;
-using VDS.RDF.Query.Expressions.Primary;
 
 namespace VDS.RDF.Query.Builder
 {
     public sealed class ExpressionBuilder
     {
-        private BooleanExpression _expression;
-        private readonly INodeFactory _nodeFactory = new NodeFactory();
-
-        public BooleanExpression Expression
-        {
-            get { return _expression; }
-            internal set { _expression = value; }
-        }
-
         public VariableExpression Variable(string variable)
         {
             return new VariableExpression(variable);
