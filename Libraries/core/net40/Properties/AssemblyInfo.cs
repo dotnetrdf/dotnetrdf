3c3ad6fb68164fd3308f890651a7cb135663d757
Rob Vesse <rvesse@dotnetrdf.org>
2012-11-26 11:57:26 -0800

    Bump revision numbers to 1930 the intended revision for the 0.8.0 release

--- Libraries/core/net40/Properties/AssemblyInfo.cs
+++ Libraries/core/net40/Properties/AssemblyInfo.cs
@@ -57,7 +57,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.0.1527")]
+[assembly: AssemblyVersion("0.8.0.1930")]
 [assembly: AssemblyFileVersion("0.8.0.0")]
 #if !NANT
 [assembly: InternalsVisibleTo("dotNetRDF.Test")]
--- Utilities/rdfConvert/Properties/AssemblyInfo.cs
+++ Utilities/rdfConvert/Properties/AssemblyInfo.cs
@@ -57,5 +57,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.0.1527")]
+[assembly: AssemblyVersion("0.7.0.1930")]
 [assembly: AssemblyFileVersion("0.7.0.0")]
