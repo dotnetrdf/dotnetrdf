57147dba6325aea4a19bc01814dbd997ddc12b5a
Rob Vesse <rvesse@dotnetrdf.org>
2012-11-29 15:54:45 -0800

    Bumping build numbers to prepare another release candidate for 0.8 release

--- Libraries/core/net40/Properties/AssemblyInfo.cs
+++ Libraries/core/net40/Properties/AssemblyInfo.cs
@@ -57,9 +57,10 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.1.1939")]
-[assembly: AssemblyFileVersion("0.8.1.1939")]
+[assembly: AssemblyVersion("0.8.2.0")]
+[assembly: AssemblyFileVersion("0.8.2.0")]
 #if !NANT
 [assembly: InternalsVisibleTo("dotNetRDF.Test")]
 #endif
+
 
