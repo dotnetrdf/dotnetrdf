2c7ef5e9d5a3dd2bc8424150bcb36b388b8c1a7c
Rob Vesse <rvesse@dotnetrdf.org>
2012-11-29 16:37:32 -0800

    Set revision to 1954 for 0.8.2 RC 3 release

--- Libraries/linkeddata/Properties/AssemblyInfo.cs
+++ Libraries/linkeddata/Properties/AssemblyInfo.cs
@@ -33,7 +33,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.*")]
-[assembly: AssemblyFileVersion("0.1.2.0")]
+[assembly: AssemblyFileVersion("0.1.2.1954")]
+
 
 
 
--- Utilities/rdfDBStudio/Properties/AssemblyInfo.cs
+++ Utilities/rdfDBStudio/Properties/AssemblyInfo.cs
@@ -51,8 +51,9 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.2.0")]
-[assembly: AssemblyFileVersion("0.1.2.0")]
+[assembly: AssemblyVersion("0.1.2.1954")]
+[assembly: AssemblyFileVersion("0.1.2.1954")]
+
 
 
 
