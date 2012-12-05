5f74f458138921dceee957264e148a50783bc1b0
Rob Vesse <rvesse@dotnetrdf.org>
2012-12-05 15:34:20 -0800

    Add test to help investigate CORE-292

---
+++ Testing/unittest/Core/TripleCollectionTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VDS.RDF.Test.Core
+{
+    [TestClass]
+    public class TripleCollectionTests
+    {
+        [TestMethod]
+        public void TripleCollectionInstantiation1()
+        {
+            TreeIndexedTripleCollection collection = new TreeIndexedTripleCollection();
+        }
+    }
+}
