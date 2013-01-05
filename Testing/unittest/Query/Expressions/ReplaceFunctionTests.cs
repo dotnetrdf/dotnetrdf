c9f4932e776caa9267ba83b6fe94c1a6ed074bb6
Tomasz Pluskiewicz <tomasz@t-code.pl>
2013-01-05 10:40:53 +0100

    test case for [CORE-299]

---
+++ Testing/unittest/Query/Expressions/ReplaceFunctionTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VDS.RDF.Nodes;
+using VDS.RDF.Query.Expressions.Functions.XPath.String;
+using VDS.RDF.Query.Expressions.Primary;
+
+namespace VDS.RDF.Query.Expressions
+{
+    [TestClass]
+    public class ReplaceFunctionTests
+    {
+        [TestMethod]
+        public void CanParalleliseShouldNotThrowWhenFindExpressionsIsConstant()
+        {
+            // when
+            var find = new ConstantTerm(new StringNode(null, "find"));
+            var replace = new VariableTerm("replacement");
+            ReplaceFunction func = new ReplaceFunction(new VariableTerm("term"), find, replace);
+
+            // then
+            var canParallelise = func.CanParallelise;
+        }
+
+        [TestMethod]
+        public void CanParalleliseShouldNotThrowWhenReplaceExpressionsIsConstant()
+        {
+            // when
+            var find = new VariableTerm("find");
+            var replace = new ConstantTerm(new StringNode(null, "replacement"));
+            ReplaceFunction func = new ReplaceFunction(new VariableTerm("term"), find, replace);
+
+            // then
+            var canParallelise = func.CanParallelise;
+        }
+    }
+}
