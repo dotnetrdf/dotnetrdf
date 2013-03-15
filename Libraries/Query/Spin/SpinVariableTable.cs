34e4d7660cf141f4d3b49d0a141bfa5a1d6aba3a
Rob Vesse <rvesse@dotnetrdf.org>
2013-03-15 11:20:02 -0700

    Move SPIN stub over from experimental branch

---
+++ Libraries/Query/Spin/SpinVariableTable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VDS.RDF.Query.Spin
+{
+    internal class SpinVariableTable
+    {
+        private Dictionary<String, INode> _vars = new Dictionary<string, INode>();
+        private IGraph _g;
+
+        public SpinVariableTable(IGraph g)
+        {
+            this._g = g;
+        }
+
+        public INode this[String var]
+        {
+            get
+            {
+                if (!this._vars.ContainsKey(var))
+                {
+                    this._vars.Add(var, this._g.CreateBlankNode());
+                }
+                return this._vars[var];
+            }
+        }
+    }
+}
