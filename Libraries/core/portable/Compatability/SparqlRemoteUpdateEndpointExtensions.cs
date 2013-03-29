6665ef2e8d56fe52865cfe363cdf13f004879c71
Kal Ahmed <kal@brightstardb.com>
2013-03-29 17:08:32 +0000

    Added missing file from last commit

---
+++ Libraries/core/portable/Compatability/SparqlRemoteUpdateEndpointExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace VDS.RDF.Update
+{
+    public static class SparqlRemoteUpdateEndpointExtensions
+    {
+        public static void Update(this SparqlRemoteUpdateEndpoint endpoint, string update)
+        {
+            var wait = new AutoResetEvent(false);
+            endpoint.Update(update, state => { (state as AutoResetEvent).Set(); }, wait);
+            wait.WaitOne();
+        }
+    }
+}
