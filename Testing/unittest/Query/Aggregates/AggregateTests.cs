767612b80714343c314b55080b8578e4b0e3d917
Rob Vesse <rvesse@dotnetrdf.org>
2013-04-01 17:20:51 -0700

    Variant on same test, still not reproducible

--- Testing/unittest/Query/Aggregates/AggregateTests.cs
+++ Testing/unittest/Query/Aggregates/AggregateTests.cs
@@ -11,7 +11,7 @@ namespace VDS.RDF.Query.Aggregates
     public class AggregateTests
     {
         [TestMethod]
-        public void SparqlAggregatesMaxBug()
+        public void SparqlAggregatesMaxBug1()
         {
             try
             {
@@ -19,6 +19,61 @@ namespace VDS.RDF.Query.Aggregates
                 store.LoadFromUri(UriFactory.Create("http://semanticsage.home.lc/files/LearningStyles.rdf"));
 
                 Options.AlgebraOptimisation = false;
+
+                IGraph graph = store.ExecuteQuery(@"prefix sage:
+<http://www.semanticsage.home.lc/LearningStyles.owl#>
+prefix xsd: <http://www.w3.org/2001/XMLSchema#>
+prefix : <http://semanticsage.home.lc/files/LearningStyles.rdf#>
+
+CONSTRUCT
+{
+    ?MTech :max ?maxScore
+}
+WHERE
+{
+    SELECT ?MTech max(?max) as ?maxScore
+    WHERE
+    {
+        SELECT ?MTech ?LessonType sum(?hasValue) as ?max
+        WHERE
+        {
+            ?MTech sage:attendsLessons ?Lesson.
+            ?Lesson sage:hasLessonType ?LessonType.
+            ?MTech sage:undergoesEvaluation ?Quiz.
+            ?Quiz sage:isForLesson ?Lesson.
+            ?MTech sage:hasQuizMarks ?QuizMarks.
+            ?QuizMarks sage:belongsToQuiz ?Quiz.
+            ?QuizMarks sage:hasValue ?hasValue.
+            ?Lesson sage:inRound '1'^^xsd:int.
+        }
+        GROUP BY ?MTech ?LessonType
+    }
+    GROUP BY ?MTech
+}") as IGraph;
+                Assert.IsNotNull(graph);
+
+                NTriplesFormatter formatter = new NTriplesFormatter();
+                INode zero = (0).ToLiteral(graph);
+                foreach (Triple t in graph.Triples)
+                {
+                    Assert.IsFalse(t.Object.CompareTo(zero) == 0, "Unexpected zero value returned");
+                }
+            }
+            finally
+            {
+                Options.AlgebraOptimisation = true;
+            }
+        }
+
+        [TestMethod]
+        public void SparqlAggregatesMaxBug2()
+        {
+            try
+            {
+                TripleStore store = new TripleStore();
+                store.LoadFromUri(UriFactory.Create("http://semanticsage.home.lc/files/LearningStyles.rdf"));
+
+                Options.AlgebraOptimisation = true;
 
                 IGraph graph = store.ExecuteQuery(@"prefix sage:
 <http://www.semanticsage.home.lc/LearningStyles.owl#>
