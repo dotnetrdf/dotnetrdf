e05bbac59348932b37be753c9b9e17fdfc1bbe14
Rob Vesse <rvesse@dotnetrdf.org>
2013-04-01 17:20:03 -0700

    Add unit test to try and reproduce a bug report, not reproducible

---
+++ Testing/unittest/Query/Aggregates/AggregateTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VDS.RDF.Writing.Formatting;
+
+namespace VDS.RDF.Query.Aggregates
+{
+    [TestClass]
+    public class AggregateTests
+    {
+        [TestMethod]
+        public void SparqlAggregatesMaxBug()
+        {
+            try
+            {
+                TripleStore store = new TripleStore();
+                store.LoadFromUri(UriFactory.Create("http://semanticsage.home.lc/files/LearningStyles.rdf"));
+
+                Options.AlgebraOptimisation = false;
+
+                IGraph graph = store.ExecuteQuery(@"prefix sage:
+<http://www.semanticsage.home.lc/LearningStyles.owl#>
+prefix xsd: <http://www.w3.org/2001/XMLSchema#>
+prefix : <http://semanticsage.home.lc/files/LearningStyles.rdf#>
+
+CONSTRUCT
+{
+    ?MTech :max ?maxScore
+}
+WHERE
+{
+    SELECT ?MTech max(?max) as ?maxScore
+    WHERE
+    {
+        SELECT ?MTech ?LessonType sum(?hasValue) as ?max
+        WHERE
+        {
+            ?MTech sage:attendsLessons ?Lesson.
+            ?Lesson sage:hasLessonType ?LessonType.
+            ?MTech sage:undergoesEvaluation ?Quiz.
+            ?Quiz sage:isForLesson ?Lesson.
+            ?MTech sage:hasQuizMarks ?QuizMarks.
+            ?QuizMarks sage:belongsToQuiz ?Quiz.
+            ?QuizMarks sage:hasValue ?hasValue.
+            ?Lesson sage:inRound '1'^^xsd:int.
+        }
+        GROUP BY ?MTech ?LessonType
+    }
+    GROUP BY ?MTech
+}") as IGraph;
+                Assert.IsNotNull(graph);
+
+                NTriplesFormatter formatter = new NTriplesFormatter();
+                INode zero = (0).ToLiteral(graph);
+                foreach (Triple t in graph.Triples)
+                {
+                    Assert.IsFalse(t.Object.CompareTo(zero) == 0, "Unexpected zero value returned");
+                }
+            }
+            finally
+            {
+                Options.AlgebraOptimisation = true;
+            }
+        }
+    }
+}
