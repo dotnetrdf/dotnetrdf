7c1b7d92e99028a468440355e36d8510345e10b8
Rob Vesse <rvesse@dotnetrdf.org>
2013-04-22 10:30:27 -0700

    Failing test case for CORE-350

--- Testing/unittest/Query/SparqlOrderByTests.cs
+++ Testing/unittest/Query/SparqlOrderByTests.cs
@@ -23,9 +23,13 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VDS.RDF.Parsing;
 using VDS.RDF.Query.Ordering;
+using VDS.RDF.Writing.Formatting;
 
 namespace VDS.RDF.Query
 {
@@ -60,6 +64,46 @@ namespace VDS.RDF.Query
 
             // then
             Assert.AreEqual("name", ordering.Variables.Single());
+        }
+
+        [TestMethod]
+        public void SparqlOrderByDescendingScope()
+        {
+            //Test Case for CORE-350
+
+            IGraph g = new Graph();
+            g.NamespaceMap.AddNamespace(String.Empty, UriFactory.Create("http://example/"));
+            INode a = g.CreateUriNode(":a");
+            INode b = g.CreateUriNode(":b");
+            INode c = g.CreateUriNode(":c");
+
+            List<INode> nodes = new List<INode>() { a, b, c };
+            List<Triple> ts = new List<Triple>();
+            foreach (INode s in nodes)
+            {
+                foreach (INode p in nodes.OrderByDescending(x => x))
+                {
+                    foreach (INode o in nodes)
+                    {
+                        ts.Add(new Triple(s, p, o));
+                    }
+                }
+            }
+            g.Assert(ts);
+            Assert.AreEqual(27, g.Triples.Count);
+
+            String query = @"SELECT * WHERE { ?s ?p ?o } ORDER BY ?s DESC(?p) ?o";
+            SparqlQuery q = new SparqlQueryParser().ParseFromString(query);
+
+            SparqlResultSet results = g.ExecuteQuery(q) as SparqlResultSet;
+            Assert.IsNotNull(results);
+            Assert.AreEqual(27, results.Count);
+
+            for (int i = 0; i < ts.Count; i++)
+            {
+                Triple t = new Triple(results[i]["s"], results[i]["p"], results[i]["o"]);
+                Assert.AreEqual(ts[i], t, "Element at position " + i + " is not as expected");
+            }
         }
     }
 }
