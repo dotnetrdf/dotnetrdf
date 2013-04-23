52d7391b0cd16f121ad61a1cf666ed62e700c294
Rob Vesse <rvesse@dotnetrdf.org>
2013-04-23 10:01:07 -0700

    Add missing test file from previous EXISTS related commits Add new tests for TriX parsing to try and reproduce a reported performance issue, not reproducible with current information

---
+++ Testing/unittest/Parsing/TriXTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VDS.RDF.Writing;
+
+namespace VDS.RDF.Parsing
+{
+    [TestClass]
+    public class TriXTests
+    {
+        private TriXWriter _writer = new TriXWriter();
+        private TriXParser _parser = new TriXParser();
+
+        [TestMethod, Timeout(2500)]
+        public void ParsingTriXPerformance1()
+        {
+            //1 Graph, 100 Triples per Graph = 100 Triples total
+            this.TestTrixPerformance(1, 100);
+        }
+
+        [TestMethod, Timeout(2500)]
+        public void ParsingTriXPerformance2()
+        {
+            //10 Graphs, 1000 Triples per Graph = 10,000 Triples total
+            this.TestTrixPerformance(10, 1000);
+        }
+
+        [TestMethod, Timeout(2500)]
+        public void ParsingTriXPerformance3()
+        {
+            //1000 Graphs, 10 Triples per Graph = 10,000 Triples total
+            this.TestTrixPerformance(1000, 10);
+        }
+
+        [TestMethod, Timeout(12500)]
+        public void ParsingTriXPerformance4()
+        {
+            //1000 Graphs, 100 Triples per Graph = 100,000 Triples total
+            this.TestTrixPerformance(1000, 100);
+        }
+
+        private void TestTrixPerformance(int numGraphs, int triplesPerGraph)
+        {
+            //Generate data
+            TripleStore store = new TripleStore();
+            for (int i = 1; i <= numGraphs; i++)
+            {
+                Graph g = new Graph();
+                g.BaseUri = new Uri("http://example.org/graph/" + i);
+
+                for (int j = 1; j <= triplesPerGraph; j++)
+                {
+                    g.Assert(new Triple(g.CreateUriNode(UriFactory.Create("http://example.org/subject/" + j)), g.CreateUriNode(UriFactory.Create("http://example.org/predicate/" + j)), (j).ToLiteral(g)));
+                }
+                store.Add(g);
+            }
+
+            Console.WriteLine("Generated dataset with " + numGraphs + " named graphs (" + triplesPerGraph + " triples/graph) with a total of " + (numGraphs * triplesPerGraph) + " triples");
+
+            Stopwatch timer = new Stopwatch();
+
+            //Write out to disk
+            timer.Start();
+            this._writer.Save(store, "temp.trix");
+            timer.Stop();
+            Console.WriteLine("Took " + timer.Elapsed + " to write to disk");
+            timer.Reset();
+
+            //Read back from disk
+            TripleStore store2 = new TripleStore();
+            timer.Start();
+            this._parser.Load(store2, "temp.trix");
+            timer.Stop();
+            Console.WriteLine("Took " + timer.Elapsed + " to read from disk");
+
+            Assert.AreEqual(numGraphs * triplesPerGraph, store2.Graphs.Sum(g => g.Triples.Count));
+
+        }
+    }
+}
