6222495d21e0a8d3af3c979965561e31bd6b7a6f
Rob Vesse <rvesse@dotnetrdf.org>
2013-05-08 15:52:15 -0700

    Bump assembly versions for next dev cycle

--- Libraries/Query/Spin/Properties/AssemblyInfo.cs
+++ Libraries/Query/Spin/Properties/AssemblyInfo.cs
@@ -33,10 +33,11 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.*")]
-[assembly: AssemblyFileVersion("0.1.0.2473")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
 
 //TODO: Remove once finished
 [assembly: InternalsVisibleTo("spin-tests")]
+
 
 
 
