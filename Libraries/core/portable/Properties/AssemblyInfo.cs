9773d7cafc8cb8e181036083b14ca362df888c45
Rob Vesse <rvesse@dotnetrdf.org>
2013-06-14 12:31:35 -0700

    Bump revision to 2648 so I can push out a pre-release package for dotNetRDF with the Portable builds (CORE-303)

--- Libraries/core/portable/Properties/AssemblyInfo.cs
+++ Libraries/core/portable/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Resources;
+using System.Resources;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -26,6 +26,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.2533")]
-[assembly: AssemblyFileVersion("1.0.1.2533")]
+[assembly: AssemblyVersion("1.0.1.2648")]
+[assembly: AssemblyFileVersion("1.0.1.2648")]
 [assembly: InternalsVisibleTo("dotNetRDF.Portable.Test")]
