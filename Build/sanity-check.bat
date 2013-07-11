f6d20349c6846357bdaa784d957407e28d2134a5
Rob Vesse <rvesse@dotnetrdf.org>
2013-07-11 10:39:47 -0700

    Improve sanity check pre-commit hook script

---
+++ Build/sanity-check.bat
@@ -0,0 +1,29 @@
+@echo off
+
+set TEMP=temp.txt
+set FOUND=found.txt
+
+:: Delete working files if they exist
+if exist %TEMP% rem %TEMP%
+if exist %FOUND% rem %FOUND%
+
+:: Determine if there are unversioned files
+hg status > %TEMP%
+findstr /B /L ? %TEMP% > %FOUND%
+echo %~dp0
+set SIZE=%%~zFOUND
+
+:: Clean up working files
+rem %TEMP%
+
+:: Exit as appropriate
+if %SIZE% gtr 0 (
+  echo %~dp0FOUND
+  echo %SIZE%
+  echo Unversioned files present while attempting commit
+  type %FOUND%
+  exit /b 1
+) else (
+  echo Commit has no unversioned files
+  exit /b 0
+)
