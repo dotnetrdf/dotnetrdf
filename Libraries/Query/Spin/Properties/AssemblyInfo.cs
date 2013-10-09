d03cb8f36b3c594404b1d734817bd1dc52513390
Rob Vesse <rvesse@dotnetrdf.org>
2013-10-09 08:11:59 -0700

    Bump revision to 2809 as final release preparation

--- Libraries/Query/Spin/Properties/AssemblyInfo.cs
+++ Libraries/Query/Spin/Properties/AssemblyInfo.cs
@@ -33,10 +33,11 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.*")]
-[assembly: AssemblyFileVersion("0.1.1.2805")]
+[assembly: AssemblyFileVersion("0.1.1.2809")]
 
 //TODO: Remove once finished
 [assembly: InternalsVisibleTo("spin-tests")]
+
 
 
 
--- Libraries/core/net35/Properties/AssemblyInfo.cs
+++ Libraries/core/net35/Properties/AssemblyInfo.cs
@@ -32,8 +32,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.2805")]
-[assembly: AssemblyFileVersion("1.0.1.2805")]
+[assembly: AssemblyVersion("1.0.1.2809")]
+[assembly: AssemblyFileVersion("1.0.1.2809")]
+
 
 
 
