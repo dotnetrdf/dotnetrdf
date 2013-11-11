b6073748f6c5bdb2624b7af1ba3cecb3634faa53
Rob Vesse <rvesse@dotnetrdf.org>
2013-11-11 14:52:52 +0000

    Set version to 1.0.2.2880 which is estimated revision for 1.0.2 release

--- Libraries/Query/Spin/Properties/AssemblyInfo.cs
+++ Libraries/Query/Spin/Properties/AssemblyInfo.cs
@@ -33,10 +33,11 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.*")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("1.0.2.2880")]
 
 //TODO: Remove once finished
 [assembly: InternalsVisibleTo("spin-tests")]
+
 
 
 
