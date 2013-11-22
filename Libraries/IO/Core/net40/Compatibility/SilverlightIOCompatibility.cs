fbd08b06b40d61e2cfb8de3cfad91c0ee3629eac
Rob Vesse <rvesse@dotnetrdf.org>
2013-11-22 14:58:40 +0000

    Get Silverlight based Core library profiles to build

---
+++ Libraries/IO/Core/net40/Compatibility/SilverlightIOCompatibility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VDS.RDF.Compatibility
+{
+#if SILVERLIGHT
+    public static class SilverlightIOCompatibility
+    {
+        public static bool IsHexEncoding(String value, int index)
+        {
+            if (index + 2 >= value.Length) return false;
+            return value[0] == '%' && SparqlSpecsHelper.IsHex(value[1]) && SparqlSpecsHelper.IsHex(value[2]);
+        }
+
+        public static char HexUnescape(String value, ref int index)
+        {
+            if (index + 2 >= value.Length) throw new ArgumentException("Malformed Percent Encoded Escape");
+            if (value[index] != '%') throw new ArgumentException("Malformed Percent Encoded Escape");
+            index = index + 3;
+            return UnicodeSpecsHelper.ConvertToChar(value.Substring(index + 1, 2));
+        }
+    }
+#endif
+}
--- Libraries/core/net40/Compatability/SilverlightCompatability.cs
+++ Libraries/core/net40/Compatability/SilverlightCompatability.cs
@@ -134,20 +134,6 @@ namespace VDS.RDF
             return u.AbsolutePath + u.Query;
         }
 
-        public static bool IsHexEncoding(String value, int index)
-        {
-            if (index + 2 >= value.Length) return false;
-            return value[0] == '%' && SparqlSpecsHelper.IsHex(value[1]) && SparqlSpecsHelper.IsHex(value[2]);
-        }
-
-        public static char HexUnescape(String value, ref int index)
-        {
-            if (index + 2 >= value.Length) throw new ArgumentException("Malformed Percent Encoded Escape");
-            if (value[index] != '%') throw new ArgumentException("Malformed Percent Encoded Escape");
-            index = index + 3;
-            return UnicodeSpecsHelper.ConvertToChar(value.Substring(index + 1, 2));
-        }
-
         /// <summary>
         /// Removes all items from a List which return true for the given function
         /// </summary>
