b79e0c920a7c4e3877a2f65da9c860f9d2052d09
Rob Vesse <rvesse@dotnetrdf.org>
2013-12-06 17:32:24 +0000

    Stub out design for a reusable Connection Management control (CORE-377)

---
+++ Utilities/storemanager/Controls/ConnectionManagementListView.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace VDS.RDF.Utilities.StoreManager.Controls
+{
+    partial class ConnectionManagementListView
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tlpLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.lvwConnections = new System.Windows.Forms.ListView();
+            this.colName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tlpLayout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tlpLayout
+            // 
+            this.tlpLayout.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tlpLayout.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tlpLayout.ColumnCount = 1;
+            this.tlpLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpLayout.Controls.Add(this.lvwConnections, 0, 0);
+            this.tlpLayout.Location = new System.Drawing.Point(3, 3);
+            this.tlpLayout.Name = "tlpLayout";
+            this.tlpLayout.RowCount = 1;
+            this.tlpLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpLayout.Size = new System.Drawing.Size(439, 264);
+            this.tlpLayout.TabIndex = 0;
+            // 
+            // lvwConnections
+            // 
+            this.lvwConnections.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvwConnections.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colName,
+            this.colType});
+            this.lvwConnections.GridLines = true;
+            this.lvwConnections.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.lvwConnections.Location = new System.Drawing.Point(3, 3);
+            this.lvwConnections.Name = "lvwConnections";
+            this.lvwConnections.Size = new System.Drawing.Size(433, 258);
+            this.lvwConnections.TabIndex = 0;
+            this.lvwConnections.UseCompatibleStateImageBehavior = false;
+            this.lvwConnections.View = System.Windows.Forms.View.Details;
+            // 
+            // colName
+            // 
+            this.colName.Text = "Name";
+            this.colName.Width = 250;
+            // 
+            // colType
+            // 
+            this.colType.Text = "Type";
+            this.colType.Width = 150;
+            // 
+            // ConnectionManagementListView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tlpLayout);
+            this.Name = "ConnectionManagementListView";
+            this.Size = new System.Drawing.Size(445, 270);
+            this.tlpLayout.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpLayout;
+        private System.Windows.Forms.ListView lvwConnections;
+        private System.Windows.Forms.ColumnHeader colName;
+        private System.Windows.Forms.ColumnHeader colType;
+    }
+}
---
+++ Utilities/storemanager/Controls/ConnectionManagementListView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VDS.RDF.Utilities.StoreManager.Controls
+{
+    public partial class ConnectionManagementListView : UserControl
+    {
+        public ConnectionManagementListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
