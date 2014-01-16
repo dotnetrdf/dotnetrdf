2b5437c3f33fac7aab9c2596c241ba48c6fb6ed8
Rob Vesse <rvesse@dotnetrdf.org>
2014-01-16 15:58:32 +0000

    Set version to 1.0.3.3062 for 1.0.3 pre-release build

--- Utilities/rdfDBStudio/Properties/AssemblyInfo.cs
+++ Utilities/rdfDBStudio/Properties/AssemblyInfo.cs
@@ -50,9 +50,10 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+// [assembly: AssemblyVersion("1.0.0.3062")]
 [assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.3062")]
+
 
 
 
