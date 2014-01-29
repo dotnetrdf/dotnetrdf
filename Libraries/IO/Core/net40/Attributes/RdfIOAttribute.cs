3e5d82c4ff5d09b520342f95cfeaeaaac0e670aa
Rob Vesse <rvesse@dotnetrdf.org>
2014-01-29 04:48:23 +0000

    Stub out attributes for declaring available RDF input types in assembly attributes

---
+++ Libraries/IO/Core/net40/Attributes/RdfIOAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VDS.RDF.Attributes
+{
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true, Inherited = false)]
+    public abstract class RdfIOAttribute
+        : Attribute
+    {
+        public String SyntaxName { get; set; }
+
+        public String FormatUri { get; set; }
+
+        public String Encoding { get; set; }
+
+        public IEnumerable<String> FileExtensions { get; set; }
+
+        public String CanonicalFileExtension { get; set; }
+
+        public IEnumerable<String> MimeTypes { get; set; }
+
+        public String CanonicalMimeType { get; set; }
+
+        public Type ParserType { get; set; }
+
+        public Type WriterType { get; set; }
+    }
+}
