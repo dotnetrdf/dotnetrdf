244eaa88e015aa09fb3c9c9fab7f7720d6773314
Rob Vesse <rvesse@dotnetrdf.org>
2015-02-17 15:18:03 +0000

    Modify tests to demonstrate that CORE-440 actually demonstrates a more comprehensive bug

--- Testing/unittest/Query/SparqlReplaceTest.cs
+++ Testing/unittest/Query/SparqlReplaceTest.cs
@@ -39,22 +39,38 @@ GROUP BY ?s
 HAVING (COUNT(?p) = 1)
 ";
 
+        private const string ReplaceHavingWorkaroundQuery = @"
+SELECT (SAMPLE(?o) AS ?sampled) (REPLACE(?sampled, ""1"", ""2"") AS ?oo)
+WHERE
+{
+    ?s ?p ?o
+}
+GROUP BY ?s
+HAVING (COUNT(?p) = 1)
+";
+
         [Test]
-        public void SparqlFunctionsReplaceTest()
+        public void SparqlFunctionsReplace()
         {
             Test(ReplaceQuery);
         }
 
         [Test]
-        public void SparqlFunctionsHavingTest()
+        public void SparqlFunctionsHaving()
         {
             Test(HavingQuery);
         }
 
         [Test]
-        public void SparqlFunctionsReplaceHavingTest()
+        public void SparqlFunctionsReplaceHaving1()
         {
             Test(ReplaceHavingQuery);
+        }
+
+        [Test]
+        public void SparqlFunctionsReplaceHaving2()
+        {
+            Test(ReplaceHavingWorkaroundQuery);
         }
 
         private static void Test(string query)
@@ -68,7 +84,12 @@ HAVING (COUNT(?p) = 1)
 
             using (SparqlResultSet resultSet = (SparqlResultSet)storage.Query(query))
             {
+                TestTools.ShowResults(resultSet);
                 Assert.AreEqual(1, resultSet.Count);
+
+                SparqlResult result = resultSet[0];
+                Assert.IsTrue(result.HasBoundValue("oo"));
+                Assert.AreEqual(graph.CreateLiteralNode("2"), result["oo"]);
             }
         }
     }
