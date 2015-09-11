b571ef2a3f01f209d9feebed14341268ed9b0aef
Rob Vesse <rvesse@dotnetrdf.org>
2015-09-11 15:35:56 +0100

    Some minor changes to SPARQL IO Core

---
+++ Libraries/Sparql/IO/Core/net40/SparqlIOExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using VDS.RDF.Parsing;
+
+namespace VDS.RDF
+{
+    public static class SparqlIOExtensions
+    {
+        public static void Load(this ISparqlResultsReader parser, ISparqlResultsHandler handler, TextReader input)
+        {
+            parser.Load(handler, input, new ParserProfile());
+        }
+    }
+}
