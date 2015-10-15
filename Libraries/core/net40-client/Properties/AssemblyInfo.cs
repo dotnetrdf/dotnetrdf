cd04519213c53c68c9351c9fb2c3c7534f392234
Rob Vesse <rvesse@dotnetrdf.org>
2015-10-15 13:10:06 +0100

    Prepare for 1.0.10 dev cycle

--- Libraries/core/net40-client/Properties/AssemblyInfo.cs
+++ Libraries/core/net40-client/Properties/AssemblyInfo.cs
@@ -31,9 +31,11 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.9.3683")]
-[assembly: AssemblyVersion("1.0.9.3683")]
-[assembly: AssemblyFileVersion("1.0.9.3683")]
+// [assembly: AssemblyVersion("1.0.10.0")]
+[assembly: AssemblyVersion("1.0.10.0")]
+[assembly: AssemblyFileVersion("1.0.10.0")]
+
+
 
 
 
