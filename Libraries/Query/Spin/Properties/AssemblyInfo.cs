b282dea9735dde6bd0e1b9f6c60abd042da24909
Kal Ahmed <kal@brightstardb.com>
2016-08-04 13:31:03 +0100

    Bumped version numbers in assembly info

--- Libraries/Query/Spin/Properties/AssemblyInfo.cs
+++ Libraries/Query/Spin/Properties/AssemblyInfo.cs
@@ -31,12 +31,13 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.11.0")]
-[assembly: AssemblyVersion("1.0.11.0")]
-[assembly: AssemblyFileVersion("1.0.11.0")]
+// [assembly: AssemblyVersion("1.0.12.0")]
+[assembly: AssemblyVersion("1.0.12.0")]
+[assembly: AssemblyFileVersion("1.0.12.0")]
 
 //TODO: Remove once finished
 //[assembly: InternalsVisibleTo("spin-tests")]
+
 
 
 
