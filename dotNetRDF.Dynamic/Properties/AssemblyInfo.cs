98c14dca8ac771f512f8afac5834170864ea6a64
Samu Lang <langsamu@hotmail.com>
2018-06-23 23:17:25 +0100

    Rename.

--- dotNetRDF.Dynamic/Properties/AssemblyInfo.cs
+++ dotNetRDF.Dynamic/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Grom.NET")]
+[assembly: AssemblyTitle("dotNetRDF.Dynamic")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Grom.NET")]
+[assembly: AssemblyProduct("dotNetRDF.Dynamic")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("Grom.NET.Tests")]
+[assembly: InternalsVisibleTo("dotNetRDF.Dynamic.Tests")]
