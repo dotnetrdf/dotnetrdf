4ab261a83d61fc9c0fb5c97005839e4da7a31081
Kal Ahmed <kal.ahmed@epimorphics.com>
2020-07-18 16:38:32 +0100

    Move the default document loader to its own class

---
+++ Libraries/dotNetRDF/JsonLd/DefaultDocumentLoader.cs
@@ -0,0 +1,146 @@
+﻿/*
+// <copyright>
+// dotNetRDF is free and open source software licensed under the MIT License
+// -------------------------------------------------------------------------
+// 
+// Copyright (c) 2009-2020 dotNetRDF Project (http://dotnetrdf.org/)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is furnished
+// to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace VDS.RDF.JsonLd
+{
+    /// <summary>
+    /// Implements the default remote JSON document loader logic for the JSON-LD processor.
+    /// </summary>
+    public static class DefaultDocumentLoader
+    {
+        public static RemoteDocument LoadJson(Uri remoteRef, JsonLdLoaderOptions loaderOptions)
+        {
+            var client = new RedirectingWebClient();
+            client.Headers.Set(HttpRequestHeader.Accept, "application/ld+json;q=1.0, application/json;q=0.9, */*+json;q=0.8");
+
+            var responseString = client.DownloadString(remoteRef);
+            var contentType = client.ResponseHeaders.GetValues("Content-Type");
+            bool matchesContentType =
+                contentType != null &&
+                contentType.Any(x => x.Contains("application/json") ||
+                                     (x.Contains("application/ld+json") &&
+                                      string.IsNullOrEmpty(loaderOptions.RequestProfile)) ||
+                                     (x.Contains("application/ld+json") &&
+                                      !string.IsNullOrEmpty(loaderOptions.RequestProfile) &&
+                                      x.Contains(loaderOptions.RequestProfile)) ||
+                                     x.Contains("+json"));
+            var documentUrl = client.ResponseUri;
+            string contextLink = null;
+            if (!matchesContentType)
+            {
+                var contextLinks = ParseLinkHeaders(client.ResponseHeaders.GetValues("Link"));
+                var alternateLink = contextLinks.FirstOrDefault(link =>
+                    link.RelationTypes.Contains("alternate") && link.MediaTypes.Contains("application/ld+json"));
+                if (alternateLink != null)
+                {
+                    return LoadJson(new Uri(remoteRef, alternateLink.LinkValue), loaderOptions);
+                }
+                else
+                {
+                    throw new JsonLdProcessorException(JsonLdErrorCode.LoadingDocumentFailed,
+                        "Loading document failed. The server did not respond with a processable JSON document.");
+                }
+            }
+
+            // If content type is application/ld+json the context link header is ignored
+            if (!contentType.Any(x => x.Contains("application/ld+json")))
+            {
+                var contextLinks = ParseLinkHeaders(client.ResponseHeaders.GetValues("Link"))
+                    .Where(x => x.RelationTypes.Contains(JsonLdVocabulary.Context))
+                    .Select(x => x.LinkValue).ToList();
+                if (contextLinks.Count > 1)
+                {
+                    throw new JsonLdProcessorException(JsonLdErrorCode.MultipleContextLinkHeaders, "Multiple context link headers");
+                }
+                contextLink = contextLinks.FirstOrDefault();
+            }
+
+            var ret = new RemoteDocument
+            {
+                ContextUrl = contextLink == null ? null : new Uri(contextLink),
+                DocumentUrl = client.ResponseUri,
+                Document = JToken.Parse(responseString),
+            };
+            return ret;
+        }
+
+        private static IEnumerable<WebLink> ParseLinkHeaders(IEnumerable<string> linkHeaderValues)
+        {
+            foreach (var linkHeaderValue in linkHeaderValues)
+            {
+                var fields = linkHeaderValue.Split(';').Select(x => x.Trim());
+                var linkValue = fields.First().TrimStart('<').TrimEnd('>');
+                var relTypes = new List<string>();
+                var mediaTypes = new List<string>();
+                foreach (var field in fields)
+                {
+                    var split = field.Split(new char[] { '=' }, 2);
+                    if (split.Length == 2)
+                    {
+                        var key = split[0].Trim();
+                        var value = split[1].Trim();
+                        if (key.Equals("rel"))
+                        {
+                            value = value.Trim('"');
+                            relTypes.AddRange(value.Split(' '));
+                        }
+                        else if (key.Equals("type"))
+                        {
+                            mediaTypes.Add(value);
+                        }
+                    }
+                }
+
+                yield return new WebLink { LinkValue = linkValue, RelationTypes = relTypes, MediaTypes = mediaTypes };
+            }
+        }
+
+        internal class WebLink
+        {
+            public string LinkValue { get; set; }
+            public List<string> RelationTypes { get; set; }
+            public List<string> MediaTypes { get; set; }
+        }
+
+        private class RedirectingWebClient : WebClient
+        {
+            public Uri ResponseUri { get; private set; }
+
+            protected override WebResponse GetWebResponse(WebRequest request)
+            {
+                var webResponse = base.GetWebResponse(request);
+                ResponseUri = webResponse?.ResponseUri;
+                return webResponse;
+            }
+        }
+    }
+}
