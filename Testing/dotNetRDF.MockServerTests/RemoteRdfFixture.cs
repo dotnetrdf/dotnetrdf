70867afb746448015af2ad76a6d2f86409fc84b9
Kal <techquila@gmail.com>
2021-06-12 12:51:28 +0100

    Repro for #396

---
+++ Testing/dotNetRDF.MockServerTests/RemoteRdfFixture.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+
+namespace dotNetRDF.MockServerTests
+{
+    public class RemoteRdfFixture : IDisposable
+    {
+        public WireMockServer Server { get; }
+
+        public RemoteRdfFixture()
+        {
+            Server = WireMockServer.Start();
+            Server.Given(Request.Create()
+                .WithPath("/rvesse.ttl")
+                .UsingGet())
+                .RespondWith(Response.Create()
+                    .WithBodyFromFile(Path.Combine("resources", "rvesse.ttl"))
+                    .WithHeader("Content-Type", "text/turtle")
+                );
+        }
+        public void Dispose()
+        {
+            Server.Stop();
+            Server.Dispose();
+        }
+    }
+}
---
+++ Testing/dotNetRDF.MockServerTests/UriLoaderTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using VDS.RDF;
+using VDS.RDF.Parsing;
+using Xunit;
+
+namespace dotNetRDF.MockServerTests
+{
+    public class UriLoaderTests: IClassFixture<RemoteRdfFixture>
+    {
+        private readonly RemoteRdfFixture _serverFixture;
+
+        public UriLoaderTests(RemoteRdfFixture serverFixture)
+        {
+            _serverFixture = serverFixture;
+            UriLoader.CacheDuration = TimeSpan.FromSeconds(2);
+        }
+
+        [Fact]
+        public void CacheUpdatesFileCreationTimeOnReload()
+        {
+            var g = new Graph();
+            var uri = new Uri(_serverFixture.Server.Urls[0] + "/rvesse.ttl");
+            UriLoader.Load(g, uri);
+            Assert.True(UriLoader.IsCached(uri));
+            Thread.Sleep(UriLoader.CacheDuration + TimeSpan.FromMilliseconds(500));
+            Assert.True(UriLoader.IsCached(uri));
+            Assert.False(UriLoader.Cache.HasLocalCopy(uri, true));
+            UriLoader.Load(g, uri);
+            Assert.True(UriLoader.IsCached(uri));
+            Assert.True(UriLoader.Cache.HasLocalCopy(uri, true));
+        }
+    }
+}
