802dff9b2ce85ca3e4913e88917738ae50d2b7c1
Kal <techquila@gmail.com>
2021-10-10 16:32:21 +0100

    Initial work on RDF-Star test suite
    Add runner framework and runner for NTriples syntax tests

---
+++ Testing/dotNetRdf.TestSuite.RdfStar/ManifestTestData.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VDS.RDF.TestSuite.RdfStar
+{
+    public class ManifestTestData : IEnumerable<object[]>
+    {
+        private readonly IGraph _manifestGraph;
+        private readonly Manifest _manifest;
+
+        public ManifestTestData(Uri baseUri, string manifestPath)
+        {
+            _manifestGraph = new Graph() { BaseUri = baseUri };
+            _manifestGraph.NamespaceMap.AddNamespace("mf", UriFactory.Root.Create("http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#"));
+            _manifestGraph.NamespaceMap.AddNamespace("rdft", UriFactory.Root.Create("http://www.w3.org/ns/rdftest#"));
+            _manifestGraph.LoadFromFile(manifestPath);
+            _manifest = new Manifest(_manifestGraph, manifestPath);
+        }
+
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            IUriNode rdfNil = _manifestGraph.CreateUriNode("rdf:nil");
+            IUriNode rdfRest = _manifestGraph.CreateUriNode("rdf:rest");
+            IUriNode rdfFirst = _manifestGraph.CreateUriNode("rdf:first");
+            IEnumerable<INode> manifests = _manifestGraph.GetTriplesWithPredicateObject(_manifestGraph.CreateUriNode("rdf:type"), _manifestGraph.CreateUriNode("mf:Manifest")).Select(t => t.Subject);
+            foreach(INode manifest in manifests)
+            {
+                IEnumerable<INode> testLists = _manifestGraph.GetTriplesWithSubjectPredicate(manifest, _manifestGraph.CreateUriNode("mf:entries")).Select(t => t.Object);
+                foreach(INode testList in testLists)
+                {
+                    INode listNode = testList;
+                    while(!listNode.Equals(rdfNil))
+                    {
+                        foreach(IUriNode testNode in _manifestGraph.GetTriplesWithSubjectPredicate(listNode, rdfFirst).Select(t => t.Object).OfType<IUriNode>())
+                        {
+                            yield return new object[] {new ManifestTest(_manifest, testNode) };
+                        }
+                        listNode = _manifestGraph.GetTriplesWithSubjectPredicate(listNode, rdfRest).Select(t=>t.Object).First();
+                    }
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    public class Manifest
+    {
+        public Uri BaseUri { get; }
+        public string LocalDirectory { get; }
+        public Uri LocalManifestUri { get; }
+        public IGraph Graph { get; }
+        public Manifest(IGraph manifestGraph, string localFilePath)
+        {
+            Graph = manifestGraph;
+            BaseUri = manifestGraph.BaseUri;
+            LocalDirectory = Path.GetDirectoryName(localFilePath);
+            LocalManifestUri = new Uri(new Uri("file://"), Path.GetFullPath(localFilePath));
+        }
+
+        public string ResolveResourcePath(Uri resourcePath)
+        {
+            var relPath = BaseUri.MakeRelativeUri(resourcePath);
+            return new Uri(LocalManifestUri, relPath).LocalPath;
+        }
+    }
+    public class ManifestTest
+    {
+        public readonly Manifest Manifest;
+        public readonly IGraph Graph;
+        public readonly IUriNode TestNode;
+
+        public ManifestTest(Manifest manifest, IUriNode testNode)
+        {
+            Manifest = manifest;
+            Graph = manifest.Graph;
+            TestNode = testNode;
+        }
+
+        public string Id => TestNode.Uri.AbsoluteUri;
+
+        public string Name => Graph.GetTriplesWithSubjectPredicate(TestNode, Graph.GetUriNode("mf:name"))
+            .Select(t => t.Object).OfType<ILiteralNode>().Select(lit => lit.Value).FirstOrDefault();
+
+        public Uri Type => Graph.GetTriplesWithSubjectPredicate(TestNode, Graph.CreateUriNode("rdf:type"))
+            .Select(t => t.Object).OfType<IUriNode>().Select(n => n.Uri).FirstOrDefault();
+
+        public Uri Action => Graph.GetTriplesWithSubjectPredicate(TestNode, Graph.CreateUriNode("mf:action"))
+            .Select(t => t.Object).OfType<IUriNode>().Select(n => n.Uri).FirstOrDefault();
+
+        public override string ToString()
+        {
+            return Id;
+        }
+    }
+
+    public class ManifestTestRunnerAttribute : Attribute
+    {
+        public readonly string TestType;
+
+        public ManifestTestRunnerAttribute(string testType)
+        {
+            TestType = testType;
+        }
+    }
+}
---
+++ Testing/dotNetRdf.TestSuite.RdfStar/NTriplesStarTestSuite.cs
@@ -0,0 +1,65 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using VDS.RDF.Parsing;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace VDS.RDF.TestSuite.RdfStar
+{
+
+    public class NTriplesStarTestSuite
+    {
+        public static ManifestTestData NTriplesTests = new ManifestTestData(
+            new Uri("http://example/base/manifest.ttl"),
+            Path.Combine("resources", "tests", "nt", "syntax", "manifest.ttl"));
+        private readonly ITestOutputHelper _output;
+
+        private Dictionary<string, MethodInfo> _runners;
+        public NTriplesStarTestSuite(ITestOutputHelper output)
+        {
+            _output = output;
+            _runners = new Dictionary<string, MethodInfo>();
+            foreach (var m in typeof(NTriplesStarTestSuite).GetMethods())
+            {
+                if (m.GetCustomAttribute(typeof(ManifestTestRunnerAttribute)) is ManifestTestRunnerAttribute runnerAttr)
+                {
+                    _runners[runnerAttr.TestType] = m;
+                }
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(NTriplesTests))]
+        public void RunTest(ManifestTest t)
+        {
+            _output.WriteLine($"{t.Id}: {t.Name} is a {t.Type}");
+            _runners.Should().ContainKey(t.Type.AbsoluteUri,
+                because: $"test class should provide a runner method for tests of type {t.Type.AbsoluteUri}");
+            MethodInfo runner = _runners[t.Type.AbsoluteUri];
+            runner.Invoke(this, new[] { t });
+        }
+
+        [ManifestTestRunner("http://www.w3.org/ns/rdftest#TestNTriplesPositiveSyntax")]
+        public void PositiveSyntaxTest(ManifestTest t)
+        {
+            _output.WriteLine($"Load from {t.Manifest.ResolveResourcePath(t.Action)}");
+            var parser = new NTriplesParser(NTriplesSyntax.Rdf11Star);
+            var g = new Graph();
+            parser.Load(g, t.Manifest.ResolveResourcePath(t.Action));
+        }
+
+        [ManifestTestRunner("http://www.w3.org/ns/rdftest#TestNTriplesNegativeSyntax")]
+        public void NegativeSyntaxTest(ManifestTest t)
+        {
+            var parser = new NTriplesParser(NTriplesSyntax.Rdf11Star);
+            var g = new Graph();
+            Assert.ThrowsAny<RdfException>(() => parser.Load(g, t.Manifest.ResolveResourcePath(t.Action)));
+        }
+    }
+}
