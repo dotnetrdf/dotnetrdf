8a8a9158c77d3ab51b3f229d0d8b21df4fcc6ef6
Kal <techquila@gmail.com>
2022-05-14 14:54:46 +0100

    Add tests for transactions with SPARQL update

--- Testing/dotNetRDF.Connectors.Virtuoso.Tests/VirtuosoConnectorTest.cs
+++ Testing/dotNetRDF.Connectors.Virtuoso.Tests/VirtuosoConnectorTest.cs
@@ -99,6 +99,62 @@ namespace VDS.RDF.Storage
         }
 
         [SkippableFact]
+        public void TestSparqlUpdateTransactionIsolation()
+        {
+            using VirtuosoConnector connector1 = GetConnector();
+            using VirtuosoConnector connector2 = GetConnector();
+
+            var graphUri = new Uri("http://localhost/VirtuosoTest");
+            var query = "SELECT * WHERE { GRAPH ?g { <http://example.org/foo> ?p ?o .} }";
+            var testData = new Graph(graphUri);
+            FileLoader.Load(testData, Path.Combine("resources", "MergePart1.ttl"));
+            connector1.SaveGraph(testData);
+
+            connector2.Query(query).Should().BeOfType<SparqlResultSet>().Which.Should().BeEmpty();
+
+            var t = new Triple(
+                new UriNode(new Uri("http://example.org/foo")),
+                new UriNode(new Uri("http://example.org/p")),
+                new LiteralNode("o", true));
+            connector1.Begin();
+            connector1.Update("INSERT DATA { GRAPH <http://localhost/VirtuosoTest> { <http://example.org/foo> <http://example.org/p> \"o\" .}}");
+
+            connector2.Query(query).Should().BeOfType<SparqlResultSet>().Which.Should().BeEmpty();
+
+            connector1.Commit();
+
+            connector2.Query(query).Should().BeOfType<SparqlResultSet>().Which.Should().NotBeEmpty();
+        }
+
+        [SkippableFact]
+        public void TestSparqlUpdateRollback()
+        {
+            using VirtuosoConnector connector1 = GetConnector();
+            using VirtuosoConnector connector2 = GetConnector();
+
+            var graphUri = new Uri("http://localhost/VirtuosoTest");
+            var query = "SELECT * WHERE { GRAPH ?g { <http://example.org/foo> ?p ?o .} }";
+            var testData = new Graph(graphUri);
+            FileLoader.Load(testData, Path.Combine("resources", "MergePart1.ttl"));
+            connector1.SaveGraph(testData);
+
+            connector2.Query(query).Should().BeOfType<SparqlResultSet>().Which.Should().BeEmpty();
+
+            var t = new Triple(
+                new UriNode(new Uri("http://example.org/foo")),
+                new UriNode(new Uri("http://example.org/p")),
+                new LiteralNode("o", true));
+            connector1.Begin();
+            connector1.Update("INSERT DATA { GRAPH <http://localhost/VirtuosoTest> { <http://example.org/foo> <http://example.org/p> \"o\" .}}");
+
+            connector2.Query(query).Should().BeOfType<SparqlResultSet>().Which.Should().BeEmpty();
+
+            connector1.Rollback();
+
+            connector2.Query(query).Should().BeOfType<SparqlResultSet>().Which.Should().BeEmpty();
+        }
+
+        [SkippableFact]
         public void SaveGraphWillAutoCommit()
         {
             using VirtuosoConnector connector1 = GetConnector();
