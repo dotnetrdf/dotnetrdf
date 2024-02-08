ee0258403abccd94eb9258a75022d7923332995a
Daan de Schepper <daan@daandeschepper.nl>
2024-02-08 15:01:24 +0100

    initial

---
+++ LanguageTagsRepro/Program.cs
@@ -0,0 +1,39 @@
+﻿using VDS.RDF;
+using VDS.RDF.Parsing;
+using VDS.RDF.Writing;
+
+var graph = new Graph();
+
+var sourceGraph = @"
+    @prefix ex: <http://example.org/> .
+
+    ex:entity ex:has_name ""name without language tag"" .
+    ex:entity ex:has_name ""name with language tag""@en .
+";
+
+using TextReader reader = new StringReader(sourceGraph);
+var parser = new TurtleParser();
+parser.Load(graph, reader);
+
+var result = graph.ExecuteQuery(@"
+    PREFIX ex: <http://example.org/>
+
+    CONSTRUCT
+    {
+        ?entity ex:has_name ?name .
+        ?entity ex:has_name ?StringWithLanguageTag .
+        ?entity ex:has_name ?StringWithoutLanguageTag .
+    }
+    WHERE
+    {
+        ?entity ex:has_name ?name .
+
+        BIND(STRLANG(""string with language tag"", ""en"") as ?StringWithLanguageTag) .
+        BIND(""string without language tag"" as ?StringWithoutLanguageTag) .
+    }
+") as IGraph;
+
+var writer = new CompressingTurtleWriter();
+var serialized = VDS.RDF.Writing.StringWriter.Write(result, writer);
+
+Console.WriteLine(serialized);
